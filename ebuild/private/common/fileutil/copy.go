e92d5e8a7470a2c8ddf866ac3b7ece37a4e8ea8d
Shuhei Takahashi <nya@chromium.org>
2022-08-17 11:12:48 +0900

    Tool to generate BUILD

--- ebuild/private/common/fileutil/copy.go
+++ ebuild/private/common/fileutil/copy.go
@@ -11,3 +11,10 @@ func Copy(src, dst string) error {
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
+
+func CopyDir(src, dst string) error {
+	cmd := exec.Command("/usr/bin/cp", "-r", "--", src, dst)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
