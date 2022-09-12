bae88d37fba164753f913199827e166350e25e40
Shuhei Takahashi <nya@chromium.org>
2022-09-12 13:58:17 +0900

    Fix runfiles detection to work with --spawn_strategy=standalone
    Change-Id: I0d047e2c4c0320e778c4bbc49f3c69be5aebc465
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1573557
    Reviewed-by: Raul Rangel <rrangel@google.com>

--- ebuild/private/common/runfiles/runfiles.go
+++ ebuild/private/common/runfiles/runfiles.go
@@ -10,8 +10,12 @@ import (
 )
 
 func FixEnv() {
-	const envName = "RUNFILES_DIR"
-	if os.Getenv(envName) != "" {
+	const (
+		envRunfilesDir  = "RUNFILES_DIR"
+		envManifestPath = "RUNFILES_MANIFEST_FILE"
+	)
+
+	if os.Getenv(envRunfilesDir) != "" || os.Getenv(envManifestPath) != "" {
 		return
 	}
 
@@ -20,5 +24,20 @@ func FixEnv() {
 		panic(fmt.Sprintf("fixing environment variables for runfiles access: %v", err))
 	}
 
-	os.Setenv(envName, exe+".runfiles")
+	runfilesDir := exe + ".runfiles"
+	manifestPath := exe + ".runfiles_manifest"
+
+	ok := false
+	if _, err := os.Stat(runfilesDir); err == nil {
+		os.Setenv(envRunfilesDir, runfilesDir)
+		ok = true
+	}
+	if _, err := os.Stat(manifestPath); err == nil {
+		os.Setenv(envManifestPath, manifestPath)
+		ok = true
+	}
+
+	if !ok {
+		panic(fmt.Sprintf("failed to locate runfiles for %s", exe))
+	}
 }
