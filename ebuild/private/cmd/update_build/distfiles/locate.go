c2f2736b0531a34bc59a1f13d1a259e454707448
Shuhei Takahashi <nya@chromium.org>
2022-10-13 12:40:47 +0900

    refactor: Split update_build into multiple files
    Split into subpackages, introduce functions to avoid deeply nested
    loops.
    
    Change-Id: I8865890d7af8f1506e6807328d9f91adc0d3a803
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1602805
    Reviewed-by: Raul Rangel <rrangel@google.com>

---
+++ ebuild/private/cmd/update_build/distfiles/locate.go
@@ -0,0 +1,148 @@
+// Copyright 2022 The ChromiumOS Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package distfiles
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"path/filepath"
+	"regexp"
+
+	"cros.local/bazel/ebuild/private/common/depdata"
+)
+
+var allowedHosts = map[string]struct{}{
+	"commondatastorage.googleapis.com": {},
+	"storage.googleapis.com":           {},
+}
+
+var distBaseURLs = []string{
+	"https://commondatastorage.googleapis.com/chromeos-mirror/gentoo/distfiles/",
+	"https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/",
+	"https://commondatastorage.googleapis.com/chromeos-localmirror/lvfs/",
+	"https://storage.googleapis.com/chromeos-localmirror/secureshell/distfiles/",
+	"https://storage.googleapis.com/chromeos-localmirror/secureshell/releases/",
+	"https://storage.googleapis.com/chromium-nodejs/14.15.4/",
+	"https://storage.googleapis.com/chromium-nodejs/16.13.0/",
+}
+
+// The following packages don't exist in the mirrors above, but instead
+// need to be pulled from the SRC_URI. We should probably mirror these so our
+// build isn't dependent on external hosts.
+var manualDistfileMap = map[string]string{
+	"iproute2-5.16.0.tar.gz": "http://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.16.0.tar.gz",
+}
+
+func getSHA(url string) (string, string, error) {
+	log.Printf("Trying: %s", url)
+	res, err := http.Get(url)
+	if err != nil {
+		return "", "", err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode/100 != 2 {
+		return "", "", fmt.Errorf("http status %d", res.StatusCode)
+	}
+
+	// TODO: Use io.TeeReader to avoid loading full content into memory.
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", "", err
+	}
+
+	sha256Hash := sha256.Sum256(body)
+	sha512Hash := sha512.Sum512(body)
+
+	// Need to unsize the slice
+	sha256HashHex := hex.EncodeToString(sha256Hash[:])
+	sha512HashHex := hex.EncodeToString(sha512Hash[:])
+
+	return sha256HashHex, sha512HashHex, nil
+}
+
+func Locate(filename string, info *depdata.URIInfo) (*Entry, error) {
+	// Once we use bazel 6.0 we can just set integrity value on the
+	// http_file.
+	var sha256 string
+	var sha512 string
+
+	goodURI, ok := manualDistfileMap[filename]
+	if !ok {
+		for _, uri := range info.Uris {
+			parsedURI, err := url.ParseRequestURI(uri)
+			if err != nil {
+				return nil, err
+			}
+
+			if parsedURI.Scheme == "gs" {
+				log.Printf("Got gs: %s", parsedURI)
+				parsedURI.Scheme = "https"
+				parsedURI.Path = filepath.Join(parsedURI.Host, parsedURI.Path)
+				parsedURI.Host = "commondatastorage.googleapis.com"
+				uri = parsedURI.String()
+			}
+
+			if _, ok := allowedHosts[parsedURI.Host]; !ok {
+				continue
+			}
+
+			goodURI = uri
+			sha256 = info.SHA256
+			break
+		}
+	}
+
+	if goodURI != "" && sha256 == "" {
+		// We don't have a SHA256 in the Manifest, Download the file and use
+		// the SHA512 in the Manifest to verify the file, then compute the
+		// SHA256 from the downloaded file.
+		var err error
+		sha256, sha512, err = getSHA(goodURI)
+		if err != nil {
+			return nil, err
+		}
+
+		if info.SHA512 != "" && sha512 != info.SHA512 {
+			return nil, fmt.Errorf("SHA512 doesn't match for file %s: %s != %s", filename, info.SHA512, sha512)
+		}
+	} else if goodURI == "" {
+		for _, distBaseURL := range distBaseURLs {
+			url := distBaseURL + filename
+
+			var err error
+			sha256, sha512, err = getSHA(url)
+			if err != nil {
+				continue
+			}
+
+			if info.SHA512 != "" && sha512 != info.SHA512 {
+				return nil, fmt.Errorf("SHA512 doesn't match for file %s: %s != %s", filename, info.SHA512, sha512)
+			}
+
+			goodURI = url
+			break
+		}
+	}
+
+	log.Printf("Found: %s w/ sha256 %s", goodURI, sha256)
+
+	if goodURI != "" && sha256 != "" {
+		name := regexp.MustCompile(`[^A-Za-z0-9]`).ReplaceAllString(filename, "_")
+		return &Entry{
+			Filename: filename,
+			URL:      goodURI,
+			SHA256:   sha256,
+			Name:     name,
+		}, nil
+	}
+	return nil, fmt.Errorf("no distfile found for %s", filename)
+}
--- ebuild/private/cmd/update_build/main.go
+++ ebuild/private/cmd/update_build/main.go
@@ -5,27 +5,22 @@
 package main
 
 import (
-	"crypto/sha256"
-	"crypto/sha512"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
-	"regexp"
 	"sort"
 	"strings"
 	"text/template"
 
+	"github.com/urfave/cli"
+
+	"cros.local/bazel/ebuild/private/cmd/update_build/distfiles"
 	"cros.local/bazel/ebuild/private/common/depdata"
 	"cros.local/bazel/ebuild/private/common/standard/dependency"
-	"github.com/urfave/cli"
 )
 
 const ebuildExt = ".ebuild"
@@ -44,152 +39,17 @@ var blockedPackages = map[string]struct{}{
 	"shfmt": {}, // tries to access goproxy.io
 }
 
-var distBaseURLs = []string{
-	"https://commondatastorage.googleapis.com/chromeos-mirror/gentoo/distfiles/",
-	"https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/",
-	"https://commondatastorage.googleapis.com/chromeos-localmirror/lvfs/",
-	"https://storage.googleapis.com/chromeos-localmirror/secureshell/distfiles/",
-	"https://storage.googleapis.com/chromeos-localmirror/secureshell/releases/",
-	"https://storage.googleapis.com/chromium-nodejs/14.15.4/",
-	"https://storage.googleapis.com/chromium-nodejs/16.13.0/",
-}
-
-var allowedHosts = map[string]struct{}{
-	"commondatastorage.googleapis.com": {},
-	"storage.googleapis.com":           {},
-}
-
-// The following packages don't exist in the mirrors above, but instead
-// need to be pulled from the SRC_URI. We should probably mirror these so our
-// build isn't dependent on external hosts.
-var manualDistfileMap = map[string]string{
-	"iproute2-5.16.0.tar.gz": "http://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.16.0.tar.gz",
-}
-
-type distEntry struct {
-	Filename string `json:"filename"`
-	URL      string `json:"url"`
-	SHA256   string `json:"sha256"`
-	Name     string `json:"name"`
-}
-
 type ebuildInfo struct {
 	EBuildName  string
 	PackageName string
 	Category    string
-	Dists       []*distEntry
+	Dists       []*distfiles.Entry
 	PackageInfo *depdata.PackageInfo
 }
 
 type packageGroup struct {
 	PackageName string
 	Packages    []string
-}
-
-func getSHA(url string) (string, string, error) {
-	log.Printf("Trying: %s", url)
-	res, err := http.Get(url)
-	if err != nil {
-		return "", "", err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode/100 != 2 {
-		return "", "", fmt.Errorf("http status %d", res.StatusCode)
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return "", "", err
-	}
-
-	sha256Hash := sha256.Sum256(body)
-	sha512Hash := sha512.Sum512(body)
-
-	// Need to unsize the slice
-	sha256HashHex := hex.EncodeToString(sha256Hash[:])
-	sha512HashHex := hex.EncodeToString(sha512Hash[:])
-
-	return sha256HashHex, sha512HashHex, nil
-}
-
-func locateDistFile(filename string, info *depdata.URIInfo) (*distEntry, error) {
-	// Once we use bazel 6.0 we can just set integrity value on the
-	// http_file.
-	var sha256 string
-	var sha512 string
-
-	goodUri, ok := manualDistfileMap[filename]
-	if !ok {
-		for _, uri := range info.Uris {
-			parsedUri, err := url.ParseRequestURI(uri)
-			if err != nil {
-				return nil, err
-			}
-
-			if parsedUri.Scheme == "gs" {
-				log.Printf("Got gs: %s", parsedUri)
-				parsedUri.Scheme = "https"
-				parsedUri.Path = filepath.Join(parsedUri.Host, parsedUri.Path)
-				parsedUri.Host = "commondatastorage.googleapis.com"
-				uri = parsedUri.String()
-			}
-
-			if _, ok := allowedHosts[parsedUri.Host]; !ok {
-				continue
-			}
-
-			goodUri = uri
-			sha256 = info.SHA256
-			break
-		}
-	}
-
-	if goodUri != "" && sha256 == "" {
-		// We don't have a SHA256 in the Manifest, Download the file and use
-		// the SHA512 in the Manifest to verify the file, then compute the
-		// SHA256 from the downloaded file.
-		var err error
-		sha256, sha512, err = getSHA(goodUri)
-		if err != nil {
-			return nil, err
-		}
-
-		if info.SHA512 != "" && sha512 != info.SHA512 {
-			return nil, fmt.Errorf("SHA512 doesn't match for file %s: %s != %s", filename, info.SHA512, sha512)
-		}
-	} else if goodUri == "" {
-		for _, distBaseURL := range distBaseURLs {
-			url := distBaseURL + filename
-
-			var err error
-			sha256, sha512, err = getSHA(url)
-			if err != nil {
-				continue
-			}
-
-			if info.SHA512 != "" && sha512 != info.SHA512 {
-				return nil, fmt.Errorf("SHA512 doesn't match for file %s: %s != %s", filename, info.SHA512, sha512)
-			}
-
-			goodUri = url
-			break
-		}
-	}
-
-	log.Printf("Found: %s w/ sha256 %s", goodUri, sha256)
-
-	if goodUri != "" && sha256 != "" {
-		name := regexp.MustCompile(`[^A-Za-z0-9]`).ReplaceAllString(filename, "_")
-		return &distEntry{
-			Filename: filename,
-			URL:      goodUri,
-			SHA256:   sha256,
-			Name:     name,
-		}, nil
-	} else {
-		return nil, fmt.Errorf("no distfile found for %s", filename)
-	}
 }
 
 var repositoriesTemplate = template.Must(template.New("").Parse(`# Copyright 2022 The ChromiumOS Authors.
@@ -266,7 +126,7 @@ package_set(
 )
 `))
 
-func generateBuild(buildPath string, fn func(f *os.File) error) error {
+func writeToFile(buildPath string, fn func(f *os.File) error) error {
 	f, err := os.Create(buildPath)
 	if err != nil {
 		return err
@@ -276,7 +136,7 @@ func generateBuild(buildPath string, fn func(f *os.File) error) error {
 	return fn(f)
 }
 
-func updateRepositories(bzlPath string, dists []*distEntry) error {
+func generateRepositories(bzlPath string, dists []*distfiles.Entry) error {
 	if len(dists) == 0 {
 		return nil
 	}
@@ -334,19 +194,17 @@ func packageAtomsToLabels(atoms []string, overlayDirs []string) ([]string, error
 	return labels, nil
 }
 
-func getDistEntries(cachedDists map[string]*distEntry, distJSONPath string, pkgInfo *depdata.PackageInfo) ([]*distEntry, error) {
-	var dists []*distEntry
+func getDistEntries(cache *distfiles.Cache, pkgInfo *depdata.PackageInfo) ([]*distfiles.Entry, error) {
+	var dists []*distfiles.Entry
 	for filename, srcInfo := range pkgInfo.SrcUris {
 		// Check the cache first.
-		if dist, ok := cachedDists[filename]; ok {
-			if dist != nil {
-				dists = append(dists, dist)
-				continue
-			}
+		if dist, ok := cache.Get(filename); ok {
+			dists = append(dists, dist)
+			continue
 		}
 
 		log.Printf("  locating distfile %s...", filename)
-		dist, err := locateDistFile(filename, srcInfo)
+		dist, err := distfiles.Locate(filename, srcInfo)
 		if err != nil {
 			log.Fatalf("WARNING: unable to locate distfile %s: %v", filename, err)
 			// TODO: Do we want to support negative caching?
@@ -355,16 +213,7 @@ func getDistEntries(cachedDists map[string]*distEntry, distJSONPath string, pkgI
 			dists = append(dists, dist)
 		}
 
-		cachedDists[filename] = dist
-
-		// Update cache.
-		b, err := json.MarshalIndent(cachedDists, "", "  ")
-		if err != nil {
-			return nil, err
-		}
-		if err := os.WriteFile(distJSONPath, b, 0o644); err != nil {
-			return nil, err
-		}
+		cache.Set(filename, dist)
 	}
 
 	sort.Slice(dists, func(i, j int) bool {
@@ -374,22 +223,143 @@ func getDistEntries(cachedDists map[string]*distEntry, distJSONPath string, pkgI
 	return dists, nil
 }
 
-func uniqueDists(list []*distEntry) []*distEntry {
-	sort.Slice(list, func(i, j int) bool {
-		return list[i].Name < list[j].Name
-	})
+func generatePackage(ebuildDir string, pkgInfoMap depdata.PackageInfoMap, cache *distfiles.Cache) ([]*distfiles.Entry, error) {
+	v := strings.Split(ebuildDir, "/")
+	category := v[len(v)-2]
+	packageName := v[len(v)-1]
+	buildPath := filepath.Join(ebuildDir, "BUILD.bazel")
 
-	var uniqueList []*distEntry
-	var previousItem *distEntry
-	for _, dep := range list {
-		if previousItem != nil && dep.Name == previousItem.Name {
-			continue
+	pkgInfos := pkgInfoMap[fmt.Sprintf("%s/%s", category, packageName)]
+	_, blocked := blockedPackages[packageName]
+	if pkgInfos == nil || blocked {
+		if err := os.Remove(buildPath); err != nil && !errors.Is(err, os.ErrNotExist) {
+			return nil, err
 		}
-		previousItem = dep
-		uniqueList = append(uniqueList, dep)
+		return nil, nil
 	}
 
-	return uniqueList
+	log.Printf("Generating: %s", buildPath)
+
+	var ebuildInfos []ebuildInfo
+
+	// Keep the previous behavior where rust pkgs were sorted ascending
+	sort.Slice(pkgInfos, func(i, j int) bool {
+		return pkgInfos[i].Version < pkgInfos[j].Version
+	})
+
+	var allDists []*distfiles.Entry
+
+	for _, pkgInfo := range pkgInfos {
+		dists, err := getDistEntries(cache, pkgInfo)
+		if err != nil {
+			return nil, err
+		}
+
+		ebuildName := fmt.Sprintf("%s-%s.ebuild", packageName, pkgInfo.Version)
+
+		var localPackageName string
+		if len(pkgInfos) == 1 {
+			localPackageName = packageName
+		} else {
+			localPackageName = fmt.Sprintf("%s-%s", packageName, pkgInfo.Version)
+		}
+
+		ebuild := ebuildInfo{
+			EBuildName:  ebuildName,
+			PackageName: localPackageName,
+			Category:    category,
+			Dists:       dists,
+			PackageInfo: pkgInfo,
+		}
+
+		ebuildInfos = append(ebuildInfos, ebuild)
+		allDists = append(allDists, dists...)
+	}
+
+	if err := writeToFile(buildPath, func(f *os.File) error {
+		if err := buildHeaderTemplate.Execute(f, nil); err != nil {
+			return err
+		}
+
+		for _, ebuild := range ebuildInfos {
+			if err := ebuildTemplate.Execute(f, ebuild); err != nil {
+				return err
+			}
+		}
+
+		if len(ebuildInfos) > 1 {
+			var targetNames []string
+
+			for _, ebuild := range ebuildInfos {
+				targetNames = append(targetNames, ebuild.PackageName)
+			}
+
+			packageGroup := packageGroup{
+				PackageName: packageName,
+				Packages:    targetNames,
+			}
+			if err := packageGroupTemplate.Execute(f, packageGroup); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return allDists, nil
+}
+
+func generateOverlay(overlayDir string, pkgInfoMap depdata.PackageInfoMap, cache *distfiles.Cache) error {
+	var overlayDists []*distfiles.Entry
+
+	var ebuildDirs []string
+	if err := filepath.WalkDir(overlayDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			return nil
+		}
+
+		fis, err := os.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		for _, fi := range fis {
+			if strings.HasSuffix(fi.Name(), ebuildExt) {
+				ebuildDirs = append(ebuildDirs, path)
+				return fs.SkipDir
+			}
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	for _, ebuildDir := range ebuildDirs {
+		dists, err := generatePackage(ebuildDir, pkgInfoMap, cache)
+		if err != nil {
+			return err
+		}
+		overlayDists = append(overlayDists, dists...)
+	}
+
+	overlayDists = distfiles.Unique(overlayDists)
+
+	if err := generateRepositories(filepath.Join(overlayDir, "repositories.bzl"), overlayDists); err != nil {
+		return err
+	}
+	return nil
+}
+
+func generate(overlayDirs []string, pkgInfoMap depdata.PackageInfoMap, cache *distfiles.Cache) error {
+	for _, overlayDir := range overlayDirs {
+		if err := generateOverlay(overlayDir, pkgInfoMap, cache); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 var flagPackageInfoFile = &cli.StringFlag{
@@ -436,138 +406,12 @@ var app = &cli.App{
 			}
 		}
 
-		cachedDists := make(map[string]*distEntry)
-		distJSONPath := filepath.Join(workspaceDir, "bazel/data/distfiles.json")
-		if b, err := os.ReadFile(distJSONPath); err == nil {
-			if err := json.Unmarshal(b, &cachedDists); err != nil {
-				return fmt.Errorf("%s: %w", distJSONPath, err)
-			}
+		cache, err := distfiles.NewCache(filepath.Join(workspaceDir, "bazel/data/distfiles.json"))
+		if err != nil {
+			return err
 		}
 
-		for _, overlayDir := range overlayDirs {
-			var overlayDists []*distEntry
-
-			var ebuildDirs []string
-			if err := filepath.WalkDir(overlayDir, func(path string, d fs.DirEntry, err error) error {
-				if err != nil {
-					return err
-				}
-				if !d.IsDir() {
-					return nil
-				}
-
-				fis, err := os.ReadDir(path)
-				if err != nil {
-					return err
-				}
-				for _, fi := range fis {
-					if strings.HasSuffix(fi.Name(), ebuildExt) {
-						ebuildDirs = append(ebuildDirs, path)
-						return fs.SkipDir
-					}
-				}
-				return nil
-			}); err != nil {
-				return err
-			}
-
-			for _, ebuildDir := range ebuildDirs {
-				v := strings.Split(ebuildDir, "/")
-				category := v[len(v)-2]
-				packageName := v[len(v)-1]
-				buildPath := filepath.Join(ebuildDir, "BUILD.bazel")
-
-				pkgInfos := pkgInfoMap[fmt.Sprintf("%s/%s", category, packageName)]
-				_, blocked := blockedPackages[packageName]
-				if pkgInfos == nil || blocked {
-					if err := os.Remove(buildPath); err != nil && !errors.Is(err, os.ErrNotExist) {
-						return err
-					}
-					continue
-				}
-
-				log.Printf("Generating: %s", buildPath)
-
-				if err := func() error {
-
-					var ebuildInfos []ebuildInfo
-
-					// Keep the previous behavior where rust pkgs were sorted ascending
-					sort.Slice(pkgInfos, func(i, j int) bool {
-						return pkgInfos[i].Version < pkgInfos[j].Version
-					})
-
-					for _, pkgInfo := range pkgInfos {
-						dists, err := getDistEntries(cachedDists, distJSONPath, pkgInfo)
-						if err != nil {
-							return err
-						}
-
-						ebuildName := fmt.Sprintf("%s-%s.ebuild", packageName, pkgInfo.Version)
-
-						var localPackageName string
-						if len(pkgInfos) == 1 {
-							localPackageName = packageName
-						} else {
-							localPackageName = fmt.Sprintf("%s-%s", packageName, pkgInfo.Version)
-						}
-
-						ebuild := ebuildInfo{
-							EBuildName:  ebuildName,
-							PackageName: localPackageName,
-							Category:    category,
-							Dists:       dists,
-							PackageInfo: pkgInfo,
-						}
-
-						ebuildInfos = append(ebuildInfos, ebuild)
-						overlayDists = append(overlayDists, dists...)
-					}
-
-					if err := generateBuild(buildPath, func(f *os.File) error {
-						if err := buildHeaderTemplate.Execute(f, nil); err != nil {
-							return err
-						}
-
-						for _, ebuild := range ebuildInfos {
-							if err := ebuildTemplate.Execute(f, ebuild); err != nil {
-								return err
-							}
-						}
-
-						if len(ebuildInfos) > 1 {
-							var targetNames []string
-
-							for _, ebuild := range ebuildInfos {
-								targetNames = append(targetNames, ebuild.PackageName)
-							}
-
-							packageGroup := packageGroup{
-								PackageName: packageName,
-								Packages:    targetNames,
-							}
-							if err := packageGroupTemplate.Execute(f, packageGroup); err != nil {
-								return err
-							}
-						}
-
-						return nil
-					}); err != nil {
-						return err
-					}
-					return nil
-				}(); err != nil {
-					log.Printf("WARNING: Failed to generate %s: %v", buildPath, err)
-				}
-			}
-
-			overlayDists = uniqueDists(overlayDists)
-
-			if err := updateRepositories(filepath.Join(overlayDir, "repositories.bzl"), overlayDists); err != nil {
-				return err
-			}
-		}
-		return nil
+		return generate(overlayDirs, pkgInfoMap, cache)
 	},
 }
 
