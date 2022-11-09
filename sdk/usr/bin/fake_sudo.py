812ea02fb738151d501941e6727116e61681d875
Matt Stark <msta@google.com>
2022-11-09 15:35:53 +1100

    Add argument parsing to sudo.
    Also move it from /usr/local/bin/sudo to /usr/bin/sudo. It appears that build_image, at some point, explicitly invokes /usr/bin/sudo.
    
    Change-Id: Id0edfdfac49cfea4a122ba2e4eac6dfe70188f45
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1637575
    Reviewed-by: Shuhei Takahashi <nya@google.com>

---
+++ sdk/usr/bin/fake_sudo.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import dataclasses
+import logging
+import re
+import os
+import sys
+from typing import Mapping, Sequence
+
+
+# Can't use the subprocess class since we're invoking exec directly.
+@dataclasses.dataclass
+class Cmd:
+  args: Sequence[str]
+  env: Mapping[str, str]
+
+
+SUDO_PATH = '/usr/sbin:/usr/bin:/sbin:/bin'
+# It probably expects a value, and a valid one. Since we're not actually
+# changing the user, we should preserve these values.
+_PRESERVED_KEYS = re.compile('^USER|HOME|SUDO_PRESERVED_.*$')
+
+
+def parse(orig_args: Sequence[str], env: Mapping[str, str]) -> Cmd:
+  logging.info('Rewrote from: %s', ' '.join(orig_args))
+  explicit_env = {}
+  preserve_env = False
+  args = orig_args[:]
+
+  # Don't use argparse, because argparse will attempt to parse unknown args
+  # after positional arguments.
+  # >>> import argparse
+  # >>> parser = argparse.ArgumentParser()
+  # >>> parser.add_argument('-u', '--user')
+  # >>> parser.parse_known_args(['ls', '-u', '.'])
+  # (Namespace(user='.'), ['ls'])
+  while args:
+    if args[0] == '--':
+      args = args[1:]
+      break
+    elif args[0] in ('-E', '--preserve-env'):
+      preserve_env = True
+      args = args[1:]
+    elif args[0] == '-u' or args[0].startswith('--user'):
+      logging.info('Dropping user flag: %s', args[1])
+      args = args[1:] if '=' in args[0] else args[2:]
+    elif args[0].startswith('-'):
+      raise NotImplementedError('Unimplemented sudo arg: %s', args[0])
+    elif '=' in args[0]:
+      k, v = args.pop(0).split('=', 1)
+      explicit_env[k] = v
+    else:
+      break
+  logging.info('Rewrote to:   %s', ' '.join(args))
+
+  if not args:
+    raise ValueError(f'Command was empty: {orig_args}')
+
+  if preserve_env:
+    cmd_env = dict(**env)
+  else:
+    cmd_env = {}
+    for key, val in env.items():
+      if _PRESERVED_KEYS.search(key) is not None:
+        cmd_env[key] = val
+
+  # When you run sudo <command>, it looks at the /etc/sudoers config file.
+  # Some args are passed through, and some are set to explicit values.
+  # My /etc/sudoers appears to explicitly set path to this value.
+  cmd_env['PATH'] = SUDO_PATH
+
+  cmd_env.update(explicit_env)
+
+  for k, v in os.environ.items():
+    if k not in cmd_env:
+      logging.info('Removed key %s=%s', k, os.environ[k])
+    elif v != cmd_env[k]:
+      logging.info('Changed key %s: %s -> %s', k, v, cmd_env[k])
+  return Cmd(args=args, env=cmd_env)
+
+
+def main():
+  logging.basicConfig(filename='/tmp/fake_sudo.log', level=logging.INFO)
+  cmd = parse(sys.argv[1:], os.environ)
+  os.execvpe(cmd.args[0], cmd.args, cmd.env)
+
+
+if __name__ == '__main__':
+  main()
---
+++ sdk/usr/bin/fake_sudo_test.py
@@ -0,0 +1,79 @@
+from typing import Mapping, Optional, Sequence
+import unittest
+
+from chromiumos.bazel.sdk.usr.bin import fake_sudo
+
+# Stop unittest from truncating the error messages.
+unittest.util._MAX_LENGTH = 999999999
+
+_ENV = dict(USER='chronos', MYVAR='myval', PATH='old_path')
+_BASE_ENV = dict(USER='chronos', PATH=fake_sudo.SUDO_PATH)
+
+
+def layered_env(*args: Mapping[str, str], **kwargs: str):
+  result = {}
+  for arg in args:
+    result.update(arg)
+  result.update(kwargs)
+  return result
+
+
+class FakeSudoTest(unittest.TestCase):
+
+  def assert_cmd_matches(self,
+      args: Sequence[str],
+      expected_args: Sequence[str],
+      expected_env: Mapping[str, str]):
+    self.assertEqual(fake_sudo.parse(args, env=_ENV), fake_sudo.Cmd(args=expected_args,
+                                                           env=expected_env))
+
+  def test_simple(self):
+    self.assertEqual(fake_sudo.parse(['echo', 'a'], env=_ENV),
+                     fake_sudo.Cmd(args=['echo', 'a'],
+                                   env=_BASE_ENV))
+
+  def test_dashed(self):
+    self.assertEqual(fake_sudo.parse(['--', 'echo', 'a'], env=_ENV),
+                     fake_sudo.Cmd(args=['echo', 'a'],
+                                   env=_BASE_ENV))
+
+  def test_env(self):
+    self.assertEqual(fake_sudo.parse(['A=b', 'echo', 'a'], env=_ENV),
+                     fake_sudo.Cmd(args=['echo', 'a'],
+                                   env=layered_env(_BASE_ENV, A='b')))
+
+  def test_user(self):
+    self.assertEqual(fake_sudo.parse(['-u', 'root', 'echo', 'a'], env=_ENV),
+                     fake_sudo.Cmd(args=['echo', 'a'],
+                                   env=_BASE_ENV))
+
+  def test_stops_early(self):
+    self.assertEqual(fake_sudo.parse(['ls', '-u', '.'], env=_ENV),
+                     fake_sudo.Cmd(args=['ls', '-u', '.'],
+                                   env=_BASE_ENV))
+
+  def test_unknown_arg(self):
+    with self.assertRaises(NotImplementedError):
+      fake_sudo.parse(['--unknown', 'echo', 'a'], env=_ENV)
+
+  def test_no_args(self):
+    with self.assertRaises(ValueError):
+      fake_sudo.parse(['-E', '--'], env=_ENV)
+
+  def test_preserve_env(self):
+    self.assertEqual(fake_sudo.parse(['-E', 'echo', 'a'], env=_ENV),
+                     fake_sudo.Cmd(args=['echo', 'a'],
+                                   env=layered_env(_ENV, _BASE_ENV)))
+    self.assertEqual(fake_sudo.parse(
+        ['-E', 'A=b', 'echo', 'a'], env=_ENV),
+        fake_sudo.Cmd(args=['echo', 'a'],
+                      env=layered_env(_ENV, _BASE_ENV, A='b')))
+
+  def test_complex(self):
+    self.assertEqual(fake_sudo.parse(
+        ['--user=root', '-E', 'A=b', '--', 'echo', 'a'], env=_ENV),
+        fake_sudo.Cmd(args=['echo', 'a'],env=layered_env(_ENV, _BASE_ENV, A='b')))
+
+
+if __name__ == '__main__':
+  unittest.main()
