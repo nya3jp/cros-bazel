9fd7885e7fc92b81f7bea29491bba894ee606760
Shuhei Takahashi <nya@google.com>
2022-11-30 07:40:00 +0000

    Revert "Generate deps.json for the test image"
    This reverts commit 984619345d2d097303c13362dbaf3f6cfc4d99e1.
    
    Reason for revert: Broke update_build
    
    Change-Id: I2b799b115d7654ea6c6c72da78dd64a73c0551ad
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1661391
    Reviewed-by: Raul Rangel <rrangel@google.com>

--- ebuild/cmd/extract_deps/main.go
+++ ebuild/cmd/extract_deps/main.go
@@ -331,7 +331,7 @@ var flagBoard = &cli.StringFlag{
 
 var flagStart = &cli.StringSliceFlag{
 	Name:  "start",
-	Value: cli.NewStringSlice("virtual/target-os", "virtual/target-os-dev", "virtual/target-os-test"),
+	Value: cli.NewStringSlice("virtual/target-os"),
 }
 
 var app = &cli.App{
