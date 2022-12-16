e3003217a3ee21e43676fb259dd61aaded7dbe03
Shuhei Takahashi <nya@chromium.org>
2022-12-16 17:45:44 +0900

    Parse arrays in bash shell variables
    It is still TODO to implement parsing dollar-quoted strings ($'...'),
    which can appear in "set" output when array values contain special
    characters. But it is not the case with ~1400 packages we build for
    arm64-generic.
    
    Change-Id: I215c7e97e43f00f3b60fcbc9a500eb612c1b0a95
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1685343
    Reviewed-by: Raul Rangel <rrangel@google.com>

--- ebuild/private/rust/src/bash.rs
+++ ebuild/private/rust/src/bash.rs
@@ -8,11 +8,11 @@ use std::collections::HashMap;
 pub type BashVars = HashMap<String, BashValue>;
 
 /// Represents a shell variable value in bash.
-#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum BashValue {
     Scalar(String),
-    // TODO: Support array values.
-    UnsupportedArray,
+    IndexedArray(Vec<String>),
+    AssociativeArray(HashMap<String, String>),
 }
 
 /// Parses an output of `set -o posix; set` from bash to create a list of
@@ -25,14 +25,14 @@ pub(crate) fn parse_set_output(output: &str) -> Result<BashVars> {
 mod parser {
     use nom::{
         branch::alt,
-        bytes::complete::{is_not, tag, take_while, take_while1},
+        bytes::complete::{tag, take_while, take_while1},
         character::{
-            complete::{anychar, none_of},
+            complete::{anychar, multispace0, multispace1, none_of},
             is_alphabetic, is_alphanumeric,
         },
-        combinator::{eof, opt},
-        multi::{many0, many1},
-        sequence::{delimited, preceded},
+        combinator::{eof, fail, opt},
+        multi::{many0, many1, separated_list0},
+        sequence::{delimited, pair, preceded},
         IResult,
     };
 
@@ -41,6 +41,44 @@ mod parser {
     struct Assignment {
         name: String,
         value: BashValue,
+    }
+
+    struct ArrayEntry {
+        key: String,
+        value: String,
+    }
+
+    impl ArrayEntry {
+        fn to_value(entries: Vec<ArrayEntry>) -> BashValue {
+            // If all keys can be parsed as integers less than INDEX_LIMIT,
+            // convert the map to an indexed array.
+            const INDEX_LIMIT: usize = 1000;
+            let is_indexed = entries
+                .iter()
+                .all(|entry| match entry.key.parse::<usize>() {
+                    Ok(index) if index < INDEX_LIMIT => true,
+                    _ => false,
+                });
+            if is_indexed {
+                return BashValue::IndexedArray(entries.into_iter().fold(
+                    Vec::new(),
+                    |mut array, entry| {
+                        let index = entry.key.parse::<usize>().unwrap();
+                        if index >= array.len() {
+                            array.resize(index + 1, String::new());
+                        }
+                        array[index] = entry.value;
+                        array
+                    },
+                ));
+            }
+            return BashValue::AssociativeArray(
+                entries
+                    .into_iter()
+                    .map(|entry| (entry.key, entry.value))
+                    .collect(),
+            );
+        }
     }
 
     fn variable(input: &str) -> IResult<&str, String> {
@@ -52,7 +90,8 @@ mod parser {
     fn unquoted_char(input: &str) -> IResult<&str, char> {
         alt((
             // https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_02
-            none_of("|&;<>()$`\\\"\' \t\n"),
+            // Also assume `[` and `]` are quoted to easily parse arrays.
+            none_of("|&;<>()$`\\\"\' \t\n[]"),
             preceded(tag("\\"), anychar),
         ))(input)
     }
@@ -67,8 +106,27 @@ mod parser {
         Ok((input, s.to_owned()))
     }
 
+    fn double_quoted_char(input: &str) -> IResult<&str, char> {
+        // For our purpose, we don't need to handle expansion ($ and `).
+        // Just handle escapes.
+        alt((preceded(tag("\\"), anychar), none_of("\"")))(input)
+    }
+
+    fn double_quoted(input: &str) -> IResult<&str, String> {
+        let (input, ss) = delimited(tag("\""), many0(double_quoted_char), tag("\""))(input)?;
+        Ok((input, ss.into_iter().collect()))
+    }
+
+    fn dollar_quoted(input: &str) -> IResult<&str, String> {
+        // TODO: Support strings escaped as $'...'. It appears when an array
+        // variable contain special characters such as newlines.
+        let (input, _): (_, ()) = delimited(tag("$'"), fail, tag("'"))(input)?;
+        Ok((input, String::new()))
+    }
+
     fn word(input: &str) -> IResult<&str, String> {
-        let (input, units) = many1(alt((unquoted, single_quoted)))(input)?;
+        let (input, units) =
+            many1(alt((unquoted, single_quoted, double_quoted, dollar_quoted)))(input)?;
         Ok((input, units.concat()))
     }
 
@@ -77,12 +135,19 @@ mod parser {
         Ok((input, BashValue::Scalar(s.unwrap_or_default())))
     }
 
+    fn array_entry(input: &str) -> IResult<&str, ArrayEntry> {
+        let (input, key) = delimited(tag("["), word, tag("]="))(input)?;
+        let (input, value) = word(input)?;
+        Ok((input, ArrayEntry { key, value }))
+    }
+
     fn array_value(input: &str) -> IResult<&str, BashValue> {
-        // TODO: Support array values.
-        // For now, just skip to the end of the line.
-        let (input, _) = tag("(")(input)?;
-        let (input, _) = is_not("\n")(input)?;
-        Ok((input, BashValue::UnsupportedArray))
+        let (input, entries) = delimited(
+            pair(tag("("), multispace0),
+            separated_list0(multispace1, array_entry),
+            pair(multispace0, tag(")")),
+        )(input)?;
+        Ok((input, ArrayEntry::to_value(entries)))
     }
 
     fn value(input: &str) -> IResult<&str, BashValue> {
@@ -108,6 +173,8 @@ mod parser {
 
 #[cfg(test)]
 mod tests {
+    use anyhow::bail;
+
     use super::*;
 
     #[test]
@@ -124,7 +191,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_set_output_escaped() -> Result<()> {
+    fn test_parse_set_output_unquoted_escaped() -> Result<()> {
         let vars = parse_set_output("IFS=\\$\n")?;
         assert_eq!(
             vars,
@@ -142,6 +209,29 @@ mod tests {
                 "LESSOPEN".to_owned(),
                 BashValue::Scalar("|/usr/bin/lesspipe %s".to_owned())
             ),])
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_set_output_double_quoted() -> Result<()> {
+        let vars = parse_set_output("LESSOPEN=\"|/usr/bin/lesspipe %s\"\n")?;
+        assert_eq!(
+            vars,
+            BashVars::from_iter([(
+                "LESSOPEN".to_owned(),
+                BashValue::Scalar("|/usr/bin/lesspipe %s".to_owned())
+            ),])
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_set_output_double_quoted_escaped() -> Result<()> {
+        let vars = parse_set_output("IFS=\"\\$\"\n")?;
+        assert_eq!(
+            vars,
+            BashVars::from_iter([("IFS".to_owned(), BashValue::Scalar("$".to_owned())),])
         );
         Ok(())
     }
@@ -187,14 +277,78 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_set_output_arrays() -> Result<()> {
-        let vars = parse_set_output("ARRAY=([0]=\"foo\" [1]=\"bar\")\nEMPTY_ARRAY=()\n")?;
+    fn test_parse_set_output_empty_arrays() -> Result<()> {
+        let vars = parse_set_output("ARRAY=()\n")?;
         assert_eq!(
             vars,
-            BashVars::from_iter([
-                ("ARRAY".to_owned(), BashValue::UnsupportedArray),
-                ("EMPTY_ARRAY".to_owned(), BashValue::UnsupportedArray),
-            ])
+            BashVars::from_iter([("ARRAY".to_owned(), BashValue::IndexedArray(vec![])),])
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_set_output_indexed_arrays() -> Result<()> {
+        let vars = parse_set_output("ARRAY=([0]=\"foo\" [1]=\"bar\")\n")?;
+        assert_eq!(
+            vars,
+            BashVars::from_iter([(
+                "ARRAY".to_owned(),
+                BashValue::IndexedArray(vec!["foo".to_owned(), "bar".to_owned()])
+            ),])
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_set_output_indexed_arrays_sparse() -> Result<()> {
+        let vars = parse_set_output("ARRAY=([1]=\"foo\" [4]=\"bar\")\n")?;
+        assert_eq!(
+            vars,
+            BashVars::from_iter([(
+                "ARRAY".to_owned(),
+                BashValue::IndexedArray(vec![
+                    "".to_owned(),
+                    "foo".to_owned(),
+                    "".to_owned(),
+                    "".to_owned(),
+                    "bar".to_owned()
+                ])
+            ),])
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_set_output_large_indexed_arrays() -> Result<()> {
+        let vars = parse_set_output("ARRAY1=([999]=\"foo\")\nARRAY2=([1000]=\"foo\")\n")?;
+        // ARRAY1 should be IndexedArray, whereas ARRAY2 should be AssociativeArray
+        // since its key is too large.
+        match &vars["ARRAY1"] {
+            BashValue::IndexedArray(values) if values.len() == 1000 => {}
+            other => bail!("ARRAY1 has unexpected value: {:?}", other),
+        }
+        match &vars["ARRAY2"] {
+            BashValue::AssociativeArray(_) => {}
+            other => bail!("ARRAY2 has unexpected value: {:?}", other),
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_set_output_associative_arrays() -> Result<()> {
+        let vars = parse_set_output("ARRAY=([foo]=\"FOO\" [\"bar\"]='BAR')\n")?;
+        assert_eq!(
+            vars,
+            BashVars::from_iter([(
+                "ARRAY".to_owned(),
+                BashValue::AssociativeArray(
+                    [
+                        ("foo".to_owned(), "FOO".to_owned()),
+                        ("bar".to_owned(), "BAR".to_owned())
+                    ]
+                    .into()
+                )
+            ),])
         );
         Ok(())
     }
