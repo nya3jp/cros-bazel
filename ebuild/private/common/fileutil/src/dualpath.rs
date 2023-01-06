979385ce1882a1e3316cf34e5a6350d84c53afa1
Matt Stark <msta@google.com>
2023-01-06 12:45:46 +1100

    Migrate DualPath to rust.
    Change-Id: I069ebd449ec5982e45802ddc5a3a489d76aa826a
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1696195
    Reviewed-by: Shuhei Takahashi <nya@google.com>

---
+++ ebuild/private/common/fileutil/src/dualpath.rs
@@ -0,0 +1,37 @@
+use std::path::Path;
+use std::path::PathBuf;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct DualPath {
+    pub outside: PathBuf,
+    pub inside: PathBuf,
+}
+
+impl DualPath {
+    pub fn join<P: AsRef<Path>>(&self, path: P) -> DualPath {
+        Self {
+            outside: self.outside.join(&path),
+            inside: self.inside.join(&path),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dualpath_join() {
+        let dp = DualPath {
+            inside: PathBuf::from("/inside"),
+            outside: PathBuf::from("/outside"),
+        };
+        assert_eq!(
+            dp.join("blah"),
+            DualPath {
+                inside: PathBuf::from("/inside/blah"),
+                outside: PathBuf::from("/outside/blah"),
+            }
+        );
+    }
+}
