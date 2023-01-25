7161a89911a7ad5c10ea9967aa18badc8222685c
Shuhei Takahashi <nya@chromium.org>
2023-01-25 12:10:01 +0900

    Sort the entry order of src.bzl
    TEST=./regen-srcs.sh
    BUG=None
    
    Change-Id: Ia5c4a2f2d1c308e94426cea744056971c8fd855a
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1714542
    Reviewed-by: Raul Rangel <rrangel@google.com>

--- ebuild/private/alchemist/regen-srcs.sh
+++ ebuild/private/alchemist/regen-srcs.sh
@@ -14,17 +14,19 @@ cd "$(dirname -- "$0")"
 	echo "# It should be regenerated each time a file is added or removed."
 
 	echo "ALCHEMIST_SRCS = ["
-	# Add the workspace root Cargo.toml.
-	# TODO: Check if `cargo_bootstrap_repository` derives this from the project's
-	# Cargo.toml.
-	echo '    Label("//:Cargo.toml"),'
-	find src -type f \
-		-printf '    Label("//bazel/ebuild/private/alchemist:%p"),\n'
+	(
+		# Add the workspace root Cargo.toml.
+		# TODO: Check if `cargo_bootstrap_repository` derives this from the project's
+		# Cargo.toml.
+		echo '    Label("//:Cargo.toml"),'
+		find src -type f \
+			-printf '    Label("//bazel/ebuild/private/alchemist:%p"),\n'
 
-	find ../common/standard/version \
-		-type f \
-		! \( -name '*.go' -or -name 'BUILD.bazel' \) \
-		-printf '    Label("//bazel/ebuild/private/common/standard/version:%P"),\n'
+		find ../common/standard/version \
+			-type f \
+			! \( -name '*.go' -or -name 'BUILD.bazel' \) \
+			-printf '    Label("//bazel/ebuild/private/common/standard/version:%P"),\n'
+	) | LC_ALL=C sort
 
 	echo "]"
 ) > ./src.bzl
