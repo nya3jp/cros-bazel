da5ba9475838963a9f6aaeb9477f65c6d17e1297
Shuhei Takahashi <nya@chromium.org>
2023-01-30 11:59:01 +0900

    alchemist: Generate wrappers in /build/$BOARD/build/bin
    Notably pkg-config is needed. I'm not sure about other Portage tool
    wrappers, but let us generate them as well for better compatibility.
    
    BUG=b:262460684
    TEST=BOARD=amd64-generic bazel build @portage//chromeos-base/codelab:9999
    TEST=./regen-srcs.sh
    
    Change-Id: I78b729a1b25de72787387c4d191326fb7a1cb326
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1718747
    Reviewed-by: Raul Rangel <rrangel@google.com>

--- ebuild/private/alchemist/src/bin/alchemist/generate_repo/internal/sdk/mod.rs
+++ ebuild/private/alchemist/src/bin/alchemist/generate_repo/internal/sdk/mod.rs
@@ -21,12 +21,100 @@ use super::super::common::{AUTOGENERATE_NOTICE, CHROOT_SRC_DIR, CHROOT_THIRD_PAR
 lazy_static! {
     static ref TEMPLATES: Tera = {
         let mut tera: Tera = Default::default();
+        tera.add_raw_template("emerge", include_str!("templates/emerge"))
+            .unwrap();
         tera.add_raw_template("make.conf", include_str!("templates/make.conf"))
+            .unwrap();
+        tera.add_raw_template("pkg-config", include_str!("templates/pkg-config"))
+            .unwrap();
+        tera.add_raw_template("portage-tool", include_str!("templates/portage-tool"))
             .unwrap();
         tera.add_raw_template("sdk.BUILD.bazel", include_str!("templates/sdk.BUILD.bazel"))
             .unwrap();
         tera
     };
+}
+
+struct WrapperDef {
+    name: &'static str,
+    template: &'static str,
+}
+
+const WRAPPER_DEFS: &[WrapperDef] = &[
+    WrapperDef {
+        name: "pkg-config",
+        template: "pkg-config",
+    },
+    WrapperDef {
+        name: "emerge",
+        template: "emerge",
+    },
+    WrapperDef {
+        name: "ebuild",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "eclean",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "emaint",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "equery",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "portageq",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "qcheck",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "qdepends",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "qfile",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "qlist",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "qmerge",
+        template: "portage-tool",
+    },
+    WrapperDef {
+        name: "qsize",
+        template: "portage-tool",
+    },
+];
+
+#[derive(Serialize, Debug)]
+struct WrapperContext<'a> {
+    name: &'a str,
+    board: &'a str,
+    triple: &'a str,
+}
+
+fn generate_wrappers(board: &str, triple: &str, out: &Path) -> Result<()> {
+    for def in WRAPPER_DEFS.iter() {
+        let context = WrapperContext {
+            name: def.name,
+            board,
+            triple,
+        };
+
+        let file = File::create(out.join(def.name))?;
+        TEMPLATES.render_to(def.template, &tera::Context::from_serialize(context)?, file)?;
+    }
+
+    Ok(())
 }
 
 #[derive(Serialize, Debug)]
@@ -35,6 +123,7 @@ struct SdkTemplateContext<'a> {
     overlays: Vec<String>,
     triples: Vec<&'a str>,
     profile_path: PathBuf,
+    wrappers: Vec<&'a str>,
 }
 
 fn generate_sdk_build(
@@ -52,6 +141,8 @@ fn generate_sdk_build(
         overlays_targets.push(format!("@//{}", relative.display()));
     }
 
+    let wrappers = WRAPPER_DEFS.iter().map(|def| def.name).collect();
+
     let context = SdkTemplateContext {
         board,
         triples: toolchain_config
@@ -65,6 +156,7 @@ fn generate_sdk_build(
             .collect(),
         overlays: overlays_targets,
         profile_path,
+        wrappers,
     };
 
     let mut file = File::create(out.join("BUILD.bazel"))?;
@@ -223,6 +315,9 @@ pub fn generate_sdk(
     create_dir_all(&out)?;
 
     generate_sdk_build(board, repos, toolchain_config, &out)?;
+    if let Some(toolchain) = toolchain_config.primary() {
+        generate_wrappers(board, &toolchain.name, &out)?;
+    }
     generate_make_conf_board(repos, &out)?;
     generate_make_conf_board_setup(board, repos, toolchain_config, &out)?;
 
