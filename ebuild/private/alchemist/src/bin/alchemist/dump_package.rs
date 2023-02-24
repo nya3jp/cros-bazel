6759d213a2d46c0004bf0676c8a7f5432e981928
Raul E Rangel <rrangel@google.com>
2023-02-24 14:19:09 -0700

    alchemist: Make dump-package print a default marker
    When there are multiple versions and some of them are masked or
    unstable, it requires a bit of thought to figure out which one is the
    default. This change prints a `(default)` marker on the version line to
    make it easier to understand.
    
    BUG=b:269606353
    TEST=cargo run -- --board arm64-generic dump-package sys-apps/hwids
    Verified the correct package was selected
    
    Change-Id: Ic2da0cb854ce3de9fe8fec27a78ca0dd8bcf382c
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1753822
    Reviewed-by: Ryo Hashimoto <hashimoto@google.com>

--- ebuild/private/alchemist/src/bin/alchemist/dump_package.rs
+++ ebuild/private/alchemist/src/bin/alchemist/dump_package.rs
@@ -13,15 +13,25 @@ pub fn dump_package_main(
 ) -> Result<()> {
     for atom in atoms {
         let mut packages = resolver.find_packages(&atom)?;
+        let default = resolver.find_best_package_in(&packages)?;
+
         packages.sort_by(|a, b| a.version.cmp(&b.version));
         packages.reverse();
 
         println!("=======\t{}", atom);
 
         for details in packages {
+            let is_default = match &default {
+                Some(default) => default.ebuild_path == details.ebuild_path,
+                None => false,
+            };
             println!("Path:\t\t{}", &details.ebuild_path.to_string_lossy());
             println!("Package:\t{}", &details.package_name);
-            println!("Version:\t{}", &details.version);
+            println!(
+                "Version:\t{}{}",
+                &details.version,
+                if is_default { " (Default)" } else { "" }
+            );
             println!("Slot:\t\t{}", &details.slot);
             println!("Stability:\t{:?}", details.stability);
             println!("Masked:\t\t{}", details.masked);
