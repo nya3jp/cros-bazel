bb5a729bbaf5c7c5df9bc2454a941102267170fd
Shuhei Takahashi <nya@chromium.org>
2023-03-08 16:35:38 +0900

    build_image: Extend hacks to build the base image
    - Disable licensing
    - Disable DLC
    - Disable package size reports
    - Disable dev_install
    
    BUG=b:271929630
    TEST=BOARD=amd64-generic bazel build //:chromiumos_minimal_image
    
    Change-Id: I48b3e2ebfba4f3450b4460e99f0b37dc74482faa
    Reviewed-on: https://team-review.git.corp.google.com/c/cros-build-tiger/cros-bazel/+/1775590
    Reviewed-by: Ryo Hashimoto <hashimoto@google.com>
    Reviewed-by: Raul Rangel <rrangel@google.com>

--- ebuild/private/cmd/build_image/container_files/build_image.sh
+++ ebuild/private/cmd/build_image/container_files/build_image.sh
@@ -3,15 +3,19 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# HACK: Rewrite base_image_util.sh to skip some steps we don't support yet.
-# TODO: Remove these hacks.
+# Apply the base package override.
+readonly base_image_util_path="/mnt/host/source/src/scripts/build_library/base_image_util.sh"
 if [[ -n "${BASE_PACKAGE}" ]]; then
-  readonly base_image_util_path="/mnt/host/source/src/scripts/build_library/base_image_util.sh"
   sed -i 's,\${BASE_PACKAGE},'"${BASE_PACKAGE}"',g' "${base_image_util_path}"
-  sed -i 's,sudo "\${GCLIENT_ROOT}/chromite/licensing/licenses",: &,' "${base_image_util_path}"
-  sed -i 's,build_dlc,: &,' "${base_image_util_path}"
 fi
 unset BASE_PACKAGE
+
+# HACK: Rewrite base_image_util.sh to skip some steps we don't support yet.
+# TODO: Remove these hacks.
+sed -i 's,sudo "\${GCLIENT_ROOT}/chromite/licensing/licenses",true &,' "${base_image_util_path}"
+sed -i 's,build_dlc,true &,' "${base_image_util_path}"
+sed -i 's,create_dev_install_lists ,true &,' "${base_image_util_path}"
+sed -i 's,"\${GCLIENT_ROOT}/chromite/scripts/pkg_size",true &,' "${base_image_util_path}"
 
 /mnt/host/source/chromite/bin/build_image "$@"
 RC=$?
