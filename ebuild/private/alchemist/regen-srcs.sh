62a37e751831a607f119ddd89424419f41ddbe17
Shuhei Takahashi <nya@chromium.org>
2023-05-30 14:41:53 +0900

    alchemist: Replace tabs with spaces in regen-srcs.sh
    Otherwise `cros lint` complains.
    
    BUG=None
    TEST=repo upload
    
    Change-Id: I1b679297bec5863ba7fabc8455dc06f09c9f0525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4574345
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>

--- ebuild/private/alchemist/regen-srcs.sh
+++ ebuild/private/alchemist/regen-srcs.sh
@@ -9,27 +9,27 @@ set -eu -o pipefail
 cd "$(dirname -- "$0")"
 
 (
-	echo "# AUTO GENERATED DO NOT EDIT!"
-	echo "# Regenerate this file using ./$(basename -- "$0")"
-	echo "# It should be regenerated each time a file is added or removed."
+  echo "# AUTO GENERATED DO NOT EDIT!"
+  echo "# Regenerate this file using ./$(basename -- "$0")"
+  echo "# It should be regenerated each time a file is added or removed."
 
-	echo "ALCHEMIST_SRCS = ["
-	(
-		# Add the workspace root Cargo.toml.
-		# TODO: Check if `cargo_bootstrap_repository` derives this from the project's
-		# Cargo.toml.
-		echo '    Label("//:Cargo.toml"),'
-		find src \
-			-name testdata -prune -or \
-			-type f -printf '    Label("//bazel/ebuild/private/alchemist:%p"),\n'
+  echo "ALCHEMIST_SRCS = ["
+  (
+    # Add the workspace root Cargo.toml.
+    # TODO: Check if `cargo_bootstrap_repository` derives this from the
+    # project's Cargo.toml.
+    echo '    Label("//:Cargo.toml"),'
+    find src \
+      -name testdata -prune -or \
+      -type f -printf '    Label("//bazel/ebuild/private/alchemist:%p"),\n'
 
-		find ../common/portage/version \
-			-type f \
-			! \( -name '*.go' -or -name 'BUILD.bazel' \) \
-			-printf '    Label("//bazel/ebuild/private/common/portage/version:%P"),\n'
-	) | LC_ALL=C sort
+    find ../common/portage/version \
+      -type f \
+      ! \( -name '*.go' -or -name 'BUILD.bazel' \) \
+      -printf '    Label("//bazel/ebuild/private/common/portage/version:%P"),\n'
+  ) | LC_ALL=C sort
 
-	echo "]"
+  echo "]"
 ) > ./src.bzl
 
 echo Done
