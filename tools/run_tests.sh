7af448ab8bb9b2d776074d87725afeede75f19f2
Matt Stark <msta@google.com>
2023-06-01 12:48:06 +1000

    QOL improvements to run_tests.sh
    It can now skip either cargo, bazel, or portage tests, and can take additional configuration (eg. --config=hermetic_toolchains).
    
    BUG=None
    TEST=tools/run_tests.sh
    
    Change-Id: I1f3b98fa7a450039364dbb179d9e6c6680049a73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4576586
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Auto-Submit: Matt Stark <msta@google.com>
    Commit-Queue: Matt Stark <msta@google.com>
    Tested-by: Matt Stark <msta@google.com>

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -4,32 +4,42 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Usage: [SKIP_CARGO/BAZEL/PORTAGE_TESTS=1] run_tests.sh [bazel test args]
+# eg. SKIP_CARGO_TESTS=1 run_tests.sh --config=hermetic_toolchains
+
 echo "Running precommits" >&2
 
-export BOARD=${BOARD:-amd64-generic}
+export BOARD="${BOARD:-amd64-generic}"
 
 TARGETS=(
   //rules_cros/third_party/...
   //rules_cros/toolchains/...
   //bazel/ebuild/...
-   # Check that build_package works.
-  @portage//dev-libs/leveldb
 )
 
-# Optimization: don't bust the cache between the bazel test and bazel build.
-export CACHE_BUST_DATE="${CACHE_BUST_DATE:-$(date --iso-8601=ns)}"
+if [[ -z "${SKIP_PORTAGE_TESTS:=}" ]]; then
+  # Check that build_package works.
+  TARGETS+=( @portage//dev-libs/leveldb )
+fi
 
 set -x
 
-# We can run cargo in parallel with bazel.
-cargo test --package alchemist -- --nocapture &
+if [[ -z "${SKIP_CARGO_TESTS:=}" ]]; then
+  # We can run cargo in parallel with bazel.
+  cargo test --package alchemist -- --nocapture &
+fi
 
-# Despite the name, bazel test also builds non-test targets if they're listed.
-bazel test \
-  --test_size_filters=small \
-  --config=format \
-  --keep_going \
-  -- \
-  "${TARGETS[@]}"
+if [[ -z "${SKIP_BAZEL_TESTS:=}" ]]; then
+  # Despite the name, bazel test also builds non-test targets if they're listed.
+  bazel test \
+    --test_size_filters=small \
+    --config=format \
+    --keep_going \
+    "$@" \
+    -- \
+    "${TARGETS[@]}"
+fi
 
-wait
+if [[ -z "${SKIP_CARGO_TESTS:=}" ]]; then
+  wait
+fi
