001378f30b32d713d9881c4ddb92f8fbee6a29e8
Shuhei Takahashi <nya@chromium.org>
2023-06-07 14:31:50 +0900

    container: Add a function to enter a mount namespace
    This patch introduces enter_mount_namespace function to enter a mount
    namespace, possibly using the privileged user namespace.
    
    Users of the container crate might need to call it because the new
    container API will mount some file systems to speed up container
    spin-up, which improves efficiency when we need to run a container
    multiple times.
    
    BUG=b:286150000
    TEST=bazel test :all
    
    Change-Id: Ia8b79d53e41a613e704821de015a987db2958849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4599696
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

---
+++ ebuild/private/common/container/src/namespace.rs
@@ -0,0 +1,55 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::{ensure, Context, Result};
+use nix::{
+    errno::Errno,
+    sched::{unshare, CloneFlags},
+    unistd::{getgid, getuid},
+};
+
+fn ensure_single_threaded() -> Result<()> {
+    let entries: Vec<_> = std::fs::read_dir("/proc/self/task")?.collect::<std::io::Result<_>>()?;
+    ensure!(entries.len() == 1, "The current process is multi-threaded");
+    Ok(())
+}
+
+fn enter_unprivileged_user_namespace() -> Result<()> {
+    let uid = getuid();
+    let gid = getgid();
+    unshare(CloneFlags::CLONE_NEWUSER)
+        .with_context(|| "Failed to create an unprivileged user namespace")?;
+    std::fs::write("/proc/self/setgroups", "deny")
+        .with_context(|| "Writing /proc/self/setgroups")?;
+    std::fs::write("/proc/self/uid_map", format!("0 {uid} 1\n"))
+        .with_context(|| "Writing /proc/self/uid_map")?;
+    std::fs::write("/proc/self/gid_map", format!("0 {gid} 1\n"))
+        .with_context(|| "Writing /proc/self/gid_map")?;
+    Ok(())
+}
+
+/// Enters a mount namespace so that the current process can mount some file
+/// systems such as tmpfs.
+///
+/// If the current process is unprivileged, it also enters an unprivileged user
+/// namespace. Since multi-threaded processes cannot enter a new user namespace,
+/// it is always an error to call this function after spawning a thread,
+/// regardless of whether the current process has privilege to directly enter a
+/// mount namespace.
+pub fn enter_mount_namespace() -> Result<()> {
+    ensure_single_threaded()?;
+
+    match unshare(CloneFlags::CLONE_NEWNS) {
+        Err(err) if err == Errno::EPERM => {
+            // If the current process does not have privilege, enter an
+            // unprivileged user namespace and try it again.
+            enter_unprivileged_user_namespace()?;
+            unshare(CloneFlags::CLONE_NEWNS)
+        }
+        other => other,
+    }
+    .context("Failed to enter a mount namespace")?;
+
+    Ok(())
+}
