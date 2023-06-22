fd166f72a7a0cce626062772c4f7259eaa0b60c3
Raul E Rangel <rrangel@chromium.org>
2023-06-22 13:00:46 -0600

    sdk_install_glibc: Make installing debug symbols optional
    My cross glibc binpkg doesn't currently generate debug symbols. I need
    to figure out why. Until then, only install the debug symbols if they
    exist.
    
    BUG=b:278728702
    TEST=BOARD=arm64-generic bazel build @portage//internal/sdk/stage2/target/board
    
    Change-Id: I03aa1460bc2c2afda1d71d3c3304067d048180f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4638843
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>
    Tested-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Auto-Submit: Raul Rangel <rrangel@chromium.org>

--- ebuild/private/cmd/sdk_install_glibc/setup.sh
+++ ebuild/private/cmd/sdk_install_glibc/setup.sh
@@ -17,5 +17,9 @@ mkdir -p /tmp/libc
 tar -I "zstd -f" -x -f "/mnt/host/.sdk_install_glibc/glibc.tbz2" -C /tmp/libc
 mkdir -p "/build/${BOARD}" "/build/${BOARD}/usr/lib/debug"
 rsync --archive --hard-links /tmp/libc/usr/*-cros-linux-gnu/ "/build/${BOARD}/"
-rsync --archive --hard-links /tmp/libc/usr/lib/debug/usr/*-cros-linux-gnu/ \
-  "/build/${BOARD}/usr/lib/debug/"
+# TODO(b/278728702): Once our cross-$CTARGE/glibc package includes debug
+# symbols remove this check.
+if [[ -d /tmp/libc/usr/lib/debug/usr ]]; then
+  rsync --archive --hard-links /tmp/libc/usr/lib/debug/usr/*-cros-linux-gnu/ \
+    "/build/${BOARD}/usr/lib/debug/"
+fi
