2a0f6872cbdbca07d9703db9aacd89bc823ab748
Shuhei Takahashi <nya@chromium.org>
2023-06-26 11:17:37 +0900

    action_wrapper: Convert Cargo integration tests to Bazel tests
    Bazel is now the official way to run tests, so switch action_wrapper
    integration tests to it.
    
    BUG=b:274042271
    TEST=bazel test :all
    
    Change-Id: I978cb013b82a7059c6ffadc85c19def79eaa093d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4644875
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- ebuild/private/cmd/action_wrapper/src/main.rs
+++ ebuild/private/cmd/action_wrapper/src/main.rs
@@ -271,3 +271,143 @@ fn main() -> ExitCode {
     let result = do_main();
     handle_top_level_result(result)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::process::{ExitStatus, Stdio};
+
+    use regex::Regex;
+    use tempfile::NamedTempFile;
+
+    use super::*;
+
+    const ACTION_WRAPPER_PATH: &str = "bazel/ebuild/private/cmd/action_wrapper/action_wrapper";
+
+    #[derive(PartialEq)]
+    enum TerminationKind {
+        ExitCode(u8),
+        Signal(String),
+    }
+
+    impl std::fmt::Display for TerminationKind {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                TerminationKind::ExitCode(code) => write!(f, "{code}"),
+                TerminationKind::Signal(signal) => write!(f, "{signal}"),
+            }
+        }
+    }
+
+    struct ActionWrapperOutputs {
+        pub status: ExitStatus,
+        pub stdout: String,
+        pub stderr: String,
+        pub log: String,
+    }
+
+    fn run_action_wrapper(termination_kind: TerminationKind) -> Result<ActionWrapperOutputs> {
+        let out_file = NamedTempFile::new()?;
+
+        let mut command = Command::new(ACTION_WRAPPER_PATH);
+        command
+            .arg("--log")
+            .arg(out_file.path())
+            .arg("bazel/ebuild/private/cmd/action_wrapper/testdata/test_script.sh")
+            .arg(format!("{termination_kind}"))
+            .arg("ONE")
+            .arg("TWO");
+
+        let output = command
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .output()?;
+
+        Ok(ActionWrapperOutputs {
+            status: output.status,
+            stdout: String::from_utf8(output.stdout)?,
+            stderr: String::from_utf8(output.stderr)?,
+            log: std::fs::read_to_string(out_file.path())?,
+        })
+    }
+
+    const PROGRAM_NAME: &str = "action_wrapper";
+    const TEST_SCRIPT_OUTPUT: &str = "stdout ONE\nstderr TWO\n";
+
+    #[test]
+    fn redirected_error() -> Result<()> {
+        let log_re = Regex::new(&format!(
+            r"^{TEST_SCRIPT_OUTPUT}{PROGRAM_NAME}: Command exited with code 40 in \d+\.\d+s\n"
+        ))
+        .unwrap();
+
+        let outputs = run_action_wrapper(TerminationKind::ExitCode(40))?;
+
+        assert_eq!(outputs.status.code(), Some(40));
+        assert_eq!(outputs.stdout, "");
+        assert!(
+            log_re.is_match(&outputs.stderr),
+            "stderr: {}",
+            outputs.stderr
+        );
+        assert!(log_re.is_match(&outputs.log), "log: {}", outputs.log);
+        Ok(())
+    }
+
+    #[test]
+    fn redirected_success() -> Result<()> {
+        let log_re = Regex::new(&format!(
+            r"^{TEST_SCRIPT_OUTPUT}{PROGRAM_NAME}: Command exited with code 0 in \d+\.\d+s\n"
+        ))
+        .unwrap();
+
+        let outputs = run_action_wrapper(TerminationKind::ExitCode(0))?;
+
+        assert_eq!(outputs.status.code(), Some(0));
+        assert_eq!(outputs.stdout, "");
+        assert_eq!(outputs.stderr, "");
+        assert!(log_re.is_match(&outputs.log), "log: {}", outputs.log);
+        Ok(())
+    }
+
+    #[test]
+    fn redirected_signal() -> Result<()> {
+        let log_re = Regex::new(&format!(
+            r"^{TEST_SCRIPT_OUTPUT}{PROGRAM_NAME}: Command killed with signal SIGUSR1 in \d+\.\d+s\n"
+        ))
+        .unwrap();
+
+        let outputs = run_action_wrapper(TerminationKind::Signal(String::from("USR1")))?;
+
+        assert_eq!(outputs.status.code(), Some(128 + libc::SIGUSR1));
+        assert_eq!(outputs.stdout, "");
+        assert!(
+            log_re.is_match(&outputs.stderr),
+            "stderr: {}",
+            outputs.stderr
+        );
+        assert!(log_re.is_match(&outputs.log), "log: {}", outputs.log);
+        Ok(())
+    }
+
+    #[test]
+    fn run_without_arguments() -> Result<()> {
+        let mut command = Command::new(ACTION_WRAPPER_PATH);
+
+        let output = command
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .output()?;
+
+        assert!(!output.status.success());
+
+        let actual_printed_stderr = String::from_utf8(output.stderr)?;
+
+        // We only check a part of a long error message.
+        assert!(
+            actual_printed_stderr.contains("required arguments were not provided"),
+            "unexpected stderr: {}",
+            actual_printed_stderr
+        );
+        Ok(())
+    }
+}
