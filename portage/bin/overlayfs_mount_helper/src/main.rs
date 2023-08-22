a5d1151b796105a700e4b1d505d4edff7f3eda7f
Ryo Hashimoto <hashimoto@chromium.org>
2023-08-22 21:03:54 +0900

    Try mounting overlayfs with "nouserxattr" option first
    This is supposed to be a more robust way to detect it when it's
    necessary to add "nouserxattr".
    
    When running on a non-Ubuntu Linux distro, it just fails and falls back
    to mounting without "nouserxattr".
    
    BUG=b:296450672
    TEST=bazel build @portage//sys-kernel/linux-headers
    
    Change-Id: I765b57f0aeb4a4a220a5c16cf0939570e7f1967b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4801603
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Tested-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- portage/bin/overlayfs_mount_helper/src/main.rs
+++ portage/bin/overlayfs_mount_helper/src/main.rs
@@ -4,9 +4,22 @@
 
 //! A tiny helper to mount overlayfs.
 
-use std::{ffi::OsString, process::ExitCode};
+use std::{
+    ffi::{OsStr, OsString},
+    process::ExitCode,
+};
 
 use nix::mount::MsFlags;
+
+fn mount_overlayfs(mount_dir: &OsStr, options: &OsStr) -> nix::Result<()> {
+    nix::mount::mount(
+        Some("overlay"),
+        mount_dir,
+        Some("overlay"),
+        MsFlags::empty(),
+        Some(options),
+    )
+}
 
 fn main() -> ExitCode {
     let args: Vec<OsString> = std::env::args_os().collect();
@@ -18,13 +31,36 @@ fn main() -> ExitCode {
     let options = &args[1];
     let mount_dir = &args[2];
 
-    match nix::mount::mount(
-        Some("overlay"),
-        mount_dir.as_os_str(),
-        Some("overlay"),
-        MsFlags::empty(),
-        Some(options.as_os_str()),
-    ) {
+    // Ubuntu has applied their own patch to the kernel which changes the default behavior of
+    // overlayfs. If there is no "userxattr" or "nouserxattr" in `options`, add "nouserxattr" to
+    // make it consistent with the normal Linux kernel's default behavior. b/296450672
+    if options
+        .to_string_lossy()
+        .split(',')
+        .all(|option| option != "userxattr" && option != "nouserxattr")
+    {
+        assert!(!options.is_empty());
+        let modified_options = String::from(options.to_string_lossy()) + ",nouserxattr";
+        match mount_overlayfs(mount_dir, &OsString::from(modified_options)) {
+            Ok(()) => {
+                return ExitCode::SUCCESS;
+            }
+            Err(err) => {
+                // EINVAL may mean that this is running on a Linux distribution other than Ubuntu
+                // that doesn't support "nouserxattr" as an overlayfs mount option.
+                // In that case, ignore the error and contniue mounting without "nouserxattr"
+                if err != nix::errno::Errno::EINVAL {
+                    eprintln!(
+                        "overlayfs_mount_helper: ERROR: mount failed: {}",
+                        err.desc()
+                    );
+                    return ExitCode::FAILURE;
+                }
+            }
+        }
+    }
+
+    match mount_overlayfs(mount_dir, options) {
         Ok(()) => ExitCode::SUCCESS,
         Err(err) => {
             eprintln!(
