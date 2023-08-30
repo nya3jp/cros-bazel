b628f19f1bd82877df6c7fce5705941dbc374de6
Matt Stark <msta@google.com>
2023-08-30 17:51:37 +1000

    Add custom made simple test data for some prebuilt binary packages.
    BUG=None
    TEST=portage/tools/run_tests.sh
    
    Change-Id: I84857f8851c640fb3898502d6828c8a73c400458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4825210
    Commit-Queue: Matt Stark <msta@google.com>
    Tested-by: Matt Stark <msta@google.com>
    Auto-Submit: Matt Stark <msta@google.com>
    Reviewed-by: Tim Bain <tbain@google.com>

---
+++ portage/common/testdata/usr/include/subdir/bar.h
@@ -0,0 +1,7 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PORTAGE_COMMON_TESTDATA_USR_INCLUDE_SUBDIR_BAR_H_
+#define PORTAGE_COMMON_TESTDATA_USR_INCLUDE_SUBDIR_BAR_H_
+#endif // PORTAGE_COMMON_TESTDATA_USR_INCLUDE_SUBDIR_BAR_H_
