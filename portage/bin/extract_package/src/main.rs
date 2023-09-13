f27e8555a801a1a1cd3d989beddbba135824284b
Shuhei Takahashi <nya@chromium.org>
2023-09-13 15:13:10 +0900

    extract_package: Merge /lib and /lib64 contents
    It is possible that a Portage package provides both /lib and /lib64
    (also /usr/lib and /usr/lib64), while extract_package has a hack to
    rename /lib to /lib64. This patch handles such cases by merging the two
    directories.
    
    BUG=b:300186102
    TEST=BOARD=arm64-generic bazel build @portage//dev-libs/leveldb
    
    Change-Id: I378d1de30cde251686a4b49c3ab259365774b6ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4861923
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- portage/bin/extract_package/src/main.rs
+++ portage/bin/extract_package/src/main.rs
@@ -2,14 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use binarypackage::BinaryPackage;
 use clap::Parser;
 use cliutil::cli_main;
 use container::enter_mount_namespace;
 use durabletree::DurableTree;
-use std::{path::PathBuf, process::ExitCode};
+use std::{
+    collections::HashMap,
+    ffi::OsString,
+    io::ErrorKind,
+    os::unix::prelude::MetadataExt,
+    path::{Path, PathBuf},
+    process::ExitCode,
+};
 use vdb::{create_initial_vdb, generate_vdb_contents, get_vdb_dir};
+use walkdir::WalkDir;
 
 /// Unpacks a binary package file to generate an installed image that can be
 /// mounted as an overlayfs layer.
@@ -38,6 +46,121 @@ struct Cli {
     /// Indicates that the package is for the host.
     #[arg(long)]
     host: bool,
+}
+
+fn read_xattrs(path: &Path) -> Result<HashMap<OsString, Vec<u8>>> {
+    let mut xattrs: HashMap<OsString, Vec<u8>> = HashMap::new();
+    for name in xattr::list(path).context("Failed to list xattrs")? {
+        let value = xattr::get(path, &name)?.unwrap_or_default();
+        xattrs.insert(name, value);
+    }
+    Ok(xattrs)
+}
+
+fn ensure_directories_equivalent(source_path: &Path, target_path: &Path) -> Result<()> {
+    let source_metadata = source_path
+        .symlink_metadata()
+        .with_context(|| format!("Failed to stat {}", source_path.display()))?;
+    let target_metadata = target_path
+        .symlink_metadata()
+        .with_context(|| format!("Failed to stat {}", target_path.display()))?;
+    if source_metadata.mode() != target_metadata.mode() {
+        bail!(
+            "Failed to merge two directories: inconsistent modes: 0{:o} vs. 0{:o}: {} and {}",
+            source_metadata.mode(),
+            target_metadata.mode(),
+            source_path.display(),
+            target_path.display()
+        );
+    }
+
+    let source_xattrs = read_xattrs(source_path)
+        .with_context(|| format!("Failed to read xattrs: {}", source_path.display()))?;
+    let target_xattrs = read_xattrs(target_path)
+        .with_context(|| format!("Failed to read xattrs: {}", target_path.display()))?;
+    if source_xattrs != target_xattrs {
+        bail!(
+            "Failed to merge two directories: inconsistent xattrs: {} and {}",
+            source_path.display(),
+            target_path.display()
+        );
+    }
+
+    Ok(())
+}
+
+/// Merges a source directory to a target directory.
+///
+/// Files under the source root directory are moved to the target root directory. The source root
+/// directory must exist initially, and it will be removed upon success.
+///
+/// Here is the logic to merge two root directories, applied recursively from top to bottom:
+/// - If a file under the source root directory is missing in the target root directory, it is
+///   simply moved to the new location.
+/// - If a file under the source root directory has a corresponding file in the target root
+///   directory:
+///   - If the two files are directories with equivalent metadata, they are kept as-is.
+///     Two directories are considered to have equivalent metadata if they have the same
+///     permissions and xattrs (which may contain ownership info recorded with fakefs).
+///   - In all other cases, it is an error.
+///
+/// The logic applies to the root directory as well, so it is fine even if the target root directory
+/// does not exist initially. However, the parent directory of the target root directory must exist.
+fn merge_directories(source_root: &Path, target_root: &Path) -> Result<()> {
+    let mut walk = WalkDir::new(source_root).sort_by_file_name().into_iter();
+    // We can't use for-loops with `WalkDir::skip_current_dir`.
+    loop {
+        let source_entry = match walk.next() {
+            None => break,
+            Some(Ok(entry)) => entry,
+            Some(Err(err)) => return Err(err.into()),
+        };
+
+        let source_path = source_entry.path();
+        let target_path = &target_root.join(source_path.strip_prefix(source_root)?);
+
+        let source_type = source_entry.file_type();
+        let target_type = match target_path.symlink_metadata() {
+            Ok(metadata) => metadata.file_type(),
+            Err(err) if err.kind() == ErrorKind::NotFound => {
+                std::fs::rename(source_path, target_path).with_context(|| {
+                    format!(
+                        "Failed to rename {} to {}",
+                        source_path.display(),
+                        target_path.display()
+                    )
+                })?;
+                // Skip the directory since we've moved the entire tree.
+                walk.skip_current_dir();
+                continue;
+            }
+            Err(err) => {
+                return Err(err).context(format!("Failed to stat {}", target_path.display()))
+            }
+        };
+
+        if source_type.is_dir() && target_type.is_dir() {
+            ensure_directories_equivalent(source_path, target_path)?;
+        } else {
+            bail!(
+                "Failed to merge two directories: conflicting non-directory files: \
+                {} ({:?}) and {} ({:?})",
+                source_path.display(),
+                source_type,
+                target_path.display(),
+                target_type,
+            );
+        }
+    }
+
+    // Remove remaining directories, if any.
+    match std::fs::remove_dir_all(source_root) {
+        Ok(()) => {}
+        Err(err) if err.kind() == ErrorKind::NotFound => {}
+        Err(err) => return Err(err).context(format!("Failed to remove {}", source_root.display())),
+    }
+
+    Ok(())
 }
 
 fn do_main() -> Result<()> {
@@ -72,8 +195,13 @@ fn do_main() -> Result<()> {
         for (source, target) in [("lib", "lib64"), ("usr/lib", "usr/lib64")] {
             let source = image_dir.join(source);
             let target = image_dir.join(target);
-            if source.is_dir() {
-                std::fs::rename(&source, &target).with_context(|| {
+
+            let source_metadata = match source.symlink_metadata() {
+                Err(err) if err.kind() == ErrorKind::NotFound => continue,
+                other => other?,
+            };
+            if source_metadata.is_dir() {
+                merge_directories(&source, &target).with_context(|| {
                     format!(
                         "Failed to rename {} to {}",
                         source.display(),
@@ -93,4 +221,132 @@ fn main() -> ExitCode {
     // We want CAP_DAC_OVERRIDE to scan read-protected directories on generating CONTENTS.
     enter_mount_namespace().expect("Failed to enter a mount namespace");
     cli_main(do_main, Default::default())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        fs::{File, Permissions},
+        os::unix::prelude::PermissionsExt,
+    };
+
+    use tempfile::TempDir;
+
+    use super::*;
+
+    // Tests the case where the target directory does not exist.
+    #[test]
+    fn test_merge_directories_target_missing() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir/aaa"))?;
+
+        merge_directories(source_dir, target_dir)?;
+
+        assert!(!source_dir.try_exists()?);
+        assert!(target_dir.join("dir/aaa").is_dir());
+        Ok(())
+    }
+
+    // Tests the case where the target directory does not have collisions.
+    #[test]
+    fn test_merge_directories_no_collisions() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir/aaa"))?;
+        std::fs::create_dir_all(target_dir.join("dir/bbb"))?;
+
+        merge_directories(source_dir, target_dir)?;
+
+        assert!(!source_dir.try_exists()?);
+        assert!(target_dir.join("dir/aaa").is_dir());
+        assert!(target_dir.join("dir/bbb").is_dir());
+        Ok(())
+    }
+
+    // Tests the case where the target directory have identical directories.
+    #[test]
+    fn test_merge_directories_identical_directories() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir/aaa"))?;
+        std::fs::create_dir_all(target_dir.join("dir/aaa"))?;
+
+        merge_directories(source_dir, target_dir)?;
+
+        assert!(!source_dir.try_exists()?);
+        assert!(target_dir.join("dir/aaa").is_dir());
+        Ok(())
+    }
+
+    // Tests the case where the target directory have directories with inconsistent permissions.
+    #[test]
+    fn test_merge_directories_inconsistent_permissions() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir/aaa"))?;
+        std::fs::create_dir_all(target_dir.join("dir/aaa"))?;
+        std::fs::set_permissions(source_dir.join("dir/aaa"), Permissions::from_mode(0o705))?;
+
+        assert!(merge_directories(source_dir, target_dir).is_err());
+        Ok(())
+    }
+
+    // Tests the case where the target directory have directories with inconsistent xattrs.
+    #[test]
+    fn test_merge_directories_inconsistent_xattrs() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir/aaa"))?;
+        std::fs::create_dir_all(target_dir.join("dir/aaa"))?;
+        xattr::set(
+            source_dir.join("dir/aaa"),
+            "user.extract_package_test.foo",
+            &[],
+        )?;
+
+        assert!(merge_directories(source_dir, target_dir).is_err());
+        Ok(())
+    }
+
+    // Tests the case where the target directory have conflicting regular files.
+    #[test]
+    fn test_merge_directories_conflicting_regular_files() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir"))?;
+        File::create(source_dir.join("dir/zzz"))?;
+        std::fs::create_dir_all(target_dir.join("dir"))?;
+        File::create(target_dir.join("dir/zzz"))?;
+
+        assert!(merge_directories(source_dir, target_dir).is_err());
+        Ok(())
+    }
+
+    // Tests the case where the target directory have conflicting files of different types.
+    #[test]
+    fn test_merge_directories_conflicting_typed_files() -> Result<()> {
+        let temp_dir = TempDir::new()?;
+        let source_dir = &temp_dir.path().join("source");
+        let target_dir = &temp_dir.path().join("target");
+
+        std::fs::create_dir_all(source_dir.join("dir"))?;
+        File::create(source_dir.join("dir/ppp"))?;
+        std::fs::create_dir_all(target_dir.join("dir/ppp"))?;
+
+        assert!(merge_directories(source_dir, target_dir).is_err());
+        Ok(())
+    }
 }
