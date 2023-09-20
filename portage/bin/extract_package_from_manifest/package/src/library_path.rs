9affd616c326591ec58bbcd62c9b6a4782f1c076
Matt Stark <msta@google.com>
2023-09-20 16:51:25 +1000

    Make ld_library_path a pathbuf, rather than a path.
    This allows us to mutate the packages after retrieving
    ld_library_path.
    
    BUG=None
    TEST=portage/tools/run_tests.sh
    
    Change-Id: I81adcba6764b54069a075c24df4a68e8ba917956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4876505
    Auto-Submit: Matt Stark <msta@google.com>
    Reviewed-by: Tim Bain <tbain@google.com>
    Commit-Queue: Tim Bain <tbain@google.com>
    Tested-by: Matt Stark <msta@google.com>

--- portage/bin/extract_package_from_manifest/package/src/library_path.rs
+++ portage/bin/extract_package_from_manifest/package/src/library_path.rs
@@ -6,7 +6,7 @@ use anyhow::{bail, Result};
 use regex::Regex;
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 use std::iter::Iterator;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 /// Filters paths to only paths that look like paths to shared libraries.
 fn filter_shared_lib_filenames<'a>(paths: &[&'a Path]) -> Vec<&'a Path> {
@@ -29,20 +29,20 @@ fn filter_shared_lib_filenames<'a>(paths: &[&'a Path]) -> Vec<&'a Path> {
 pub(crate) fn generate_ld_library_path<'a>(
     paths: &[&'a Path],
     directory_regexes: &[Regex],
-) -> Result<Vec<&'a Path>> {
+) -> Result<Vec<PathBuf>> {
     let paths = filter_shared_lib_filenames(paths);
-    let dirs: HashSet<&'a Path> = paths.iter().map(|p| p.parent().unwrap()).collect();
+    let dirs: HashSet<&Path> = paths.iter().map(|p| p.parent().unwrap()).collect();
 
     // A mapping from index in LD_LIBRARY_PATH to the value.
     // Order matters because the index is like a priority.
-    let mut library_paths: BTreeMap<usize, &'a Path> = BTreeMap::new();
+    let mut library_paths: BTreeMap<usize, PathBuf> = BTreeMap::new();
     for dir in dirs {
         let dir_str = dir.to_string_lossy();
         for (idx, matcher) in directory_regexes.iter().enumerate() {
             if let Some(m) = matcher.find(&dir_str) {
                 // Verify that we matched the whole directory, not just the first part.
                 if m.as_str() == &dir_str {
-                    if let Some(old_dir) = library_paths.insert(idx, dir) {
+                    if let Some(old_dir) = library_paths.insert(idx, dir.to_path_buf()) {
                         bail!(
                             "{:?} matched both {dir:?} and {old_dir:?}. \
                             Each entry must match a single path.",
@@ -64,7 +64,7 @@ pub(crate) fn generate_ld_library_path<'a>(
 ///   ["/lib64", "/lib32"]
 /// ) -> "/lib64/libfoo.so"`
 /// since lib64 comes before lib32 in the LD_LIBRARY_PATH.
-fn choose_library<'a>(files: &[&'a Path], ld_library_path: &[&Path]) -> Option<&'a Path> {
+fn choose_library<'a>(files: &[&'a Path], ld_library_path: &[PathBuf]) -> Option<&'a Path> {
     files
         .iter()
         .flat_map(|path| {
@@ -100,7 +100,7 @@ fn choose_library<'a>(files: &[&'a Path], ld_library_path: &[&Path]) -> Option<&
 /// /lib64/libfoo.so similar to how it would be masked by LD_LIBRARY_PATH.
 pub(crate) fn calculate_shared_libraries<'a>(
     paths: &[&'a Path],
-    ld_library_path: &[&Path],
+    ld_library_path: &[PathBuf],
 ) -> Result<BTreeSet<&'a Path>> {
     let paths = filter_shared_lib_filenames(paths);
 
@@ -121,35 +121,38 @@ mod tests {
 
     #[test]
     fn test_library_selection() {
-        let lib64 = Path::new("/lib64");
-        let lib32 = Path::new("/lib32");
+        let lib64 = PathBuf::from("/lib64");
+        let lib32 = PathBuf::from("/lib32");
 
         let lib64_foo = Path::new("/lib64/libfoo.so");
         let lib32_foo = Path::new("/lib32/libfoo.so");
         let unknown_foo = Path::new("/unknown/libfoo.so");
 
-        assert_eq!(choose_library(&[&unknown_foo], &[lib64, lib32]), None);
         assert_eq!(
-            choose_library(&[&lib64_foo], &[lib64, lib32]),
+            choose_library(&[&unknown_foo], &[lib64.clone(), lib32.clone()]),
+            None
+        );
+        assert_eq!(
+            choose_library(&[&lib64_foo], &[lib64.clone(), lib32.clone()]),
             Some(lib64_foo)
         );
 
         // The one in the directory corresponding to the first entry should be returned.
         assert_eq!(
-            choose_library(&[&lib32_foo, &lib64_foo], &[&lib32, &lib64]),
+            choose_library(&[&lib32_foo, &lib64_foo], &[lib32.clone(), lib64.clone()]),
             Some(lib32_foo)
         );
         assert_eq!(
-            choose_library(&[&lib64_foo, &lib32_foo], &[&lib32, &lib64]),
+            choose_library(&[&lib64_foo, &lib32_foo], &[lib32.clone(), lib64.clone()]),
             Some(lib32_foo)
         );
 
         assert_eq!(
-            choose_library(&[&lib32_foo, &lib64_foo], &[&lib64, &lib32]),
+            choose_library(&[&lib32_foo, &lib64_foo], &[lib64.clone(), lib32.clone()]),
             Some(lib64_foo)
         );
         assert_eq!(
-            choose_library(&[&lib64_foo, &lib32_foo], &[&lib64, &lib32]),
+            choose_library(&[&lib64_foo, &lib32_foo], &[lib64.clone(), lib32.clone()]),
             Some(lib64_foo)
         );
     }
@@ -191,7 +194,7 @@ mod tests {
                 &paths,
                 &[Regex::new(r"/clang/\d+")?, Regex::new("/lib64")?,]
             )?,
-            vec![Path::new("/clang/16"), Path::new("/lib64"),]
+            vec![PathBuf::from("/clang/16"), PathBuf::from("/lib64"),]
         );
 
         Ok(())
@@ -232,7 +235,7 @@ mod tests {
                     Path::new("/lib64/libfoo.so"),
                     Path::new("/lib64/foo/libbar.so"),
                 ],
-                &[Path::new("/lib64")],
+                &[PathBuf::from("/lib64")],
             )?,
             [Path::new("/lib64/libfoo.so")].into_iter().collect(),
         );
