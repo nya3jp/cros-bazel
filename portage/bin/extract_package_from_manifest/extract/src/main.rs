b32b7dbbe4888f208281705079ab33ee1bf6c568
Matt Stark <msta@google.com>
2023-09-26 11:40:22 +1000

    Add a binary to extract from a binpkg according to a manifest.
    BUG=b:265990534
    TEST=portage/tools/run_tests.sh
    
    Change-Id: Ic622f94218a74f88dec3df80723e7e8aac1ca223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/4891874
    Tested-by: Matt Stark <msta@google.com>
    Auto-Submit: Matt Stark <msta@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

---
+++ portage/bin/extract_package_from_manifest/extract/src/main.rs
@@ -0,0 +1,78 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use anyhow::{bail, Context, Result};
+use clap::Parser;
+use cliutil::cli_main;
+use extract_package_from_manifest_package::package::Package;
+use extract_package_from_manifest_package::package_set::PackageSet;
+use regex::Regex;
+use std::{fs::File, io::BufReader, path::PathBuf, process::ExitCode};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Cli {
+    #[arg(
+        long,
+        help = "The command to execute to fix an incorrect set of files in \
+        the interface"
+    )]
+    regenerate_command: String,
+
+    #[arg(long, required = true, help = "The binary package to unpack")]
+    binpkg: PathBuf,
+
+    #[arg(
+        long,
+        required = true,
+        help = "The path to a json file containing the expected package contents"
+    )]
+    manifest: PathBuf,
+
+    #[arg(long, help = "The directory to output to")]
+    out_dir: PathBuf,
+
+    #[arg(long, help = "Equivalent to the bash variable $LD_LIBRARY_PATH.")]
+    ld_library_path: Vec<PathBuf>,
+
+    #[arg(
+        long,
+        help = "A regex matching all header file directories we care about."
+    )]
+    pub header_file_dir_regex: Vec<Regex>,
+}
+
+fn do_main() -> Result<()> {
+    let args = Cli::parse();
+
+    let mut package_set = PackageSet::create(&args.out_dir, &[args.binpkg.as_path()])?;
+    package_set.fill_headers(&args.header_file_dir_regex)?;
+    package_set.fill_shared_libraries(&args.ld_library_path)?;
+
+    let got_pkg = &package_set.into_packages()[0];
+
+    let want_pkg: Package = serde_json::from_reader(BufReader::new(
+        File::open(&args.manifest)
+            .with_context(|| format!("Failed to open {:?}", args.manifest))?,
+    ))?;
+
+    if *got_pkg != want_pkg {
+        eprintln!();
+        // A change here requires a change to
+        // extract_package_from_manifest/extract.bzl and .bazel_fix_commands.json.
+        bail!(
+            "\n\
+            Interface for binary has changed. Please run '{}'\n\
+            Consider using bazel-watcher (ibazel) to automatically \
+            apply the fix and rerun \
+            (https://github.com/bazelbuild/bazel-watcher).\n",
+            &args.regenerate_command
+        );
+    }
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    cli_main(do_main, Default::default())
+}
