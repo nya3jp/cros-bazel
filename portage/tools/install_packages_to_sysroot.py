a62e84818c0ced675c5a23e24779fe18f1b2b753
Matt Stark <msta@google.com>
2023-12-15 12:17:06 +1100

    Simplify install_packages_to_sysroot
    BUG=b:304630062
    TEST=portage/tools/run_tests.sh
    
    Force-Relevant-Builds: amd64-generic-bazel-cq
    Change-Id: Ieb1e47a49dc252034243bcdf052893fb5b38727f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5124775
    Commit-Queue: Matt Stark <msta@google.com>
    Reviewed-by: Tim Bain <tbain@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Tested-by: Matt Stark <msta@google.com>

--- portage/tools/install_packages_to_sysroot.py
+++ portage/tools/install_packages_to_sysroot.py
@@ -6,7 +6,6 @@
 """A tool to build and install packages to the sysroot."""
 
 import argparse
-import json
 import logging
 import os
 import subprocess
@@ -36,9 +35,6 @@ def main():
     args = arg_parser.parse_args()
 
     os.environ["BOARD"] = args.board
-    workspace_root = subprocess.check_output(
-        [_BAZEL_PATH, "info", "workspace"], encoding="utf-8"
-    ).strip()
 
     # Generate chromeos-chrome prebuilt config.
     chrome_prebuilt_configs = []
@@ -76,82 +72,10 @@ def main():
         ]
         + chrome_prebuilt_configs
         + [
-            "@portage//target/%s:%s" % (package_name, target_type)
-            for target_type in ["package_set", "install_list"]
+            "@portage//target/%s:installed" % package_name
             for package_name in args.package_names
         ],
     )
-
-    # Load install lists.
-    # install_lists is a list of lists whose elements are dicts like this:
-    # {
-    #   "name": "foo/bar-0.0.1.tbz2",
-    #   "path": "bazel-out/.../foo/bar/bar-0.0.1.tbz2",
-    #   "deps": ["foo/baz-0.0.1.tbz2"],
-    # }
-    install_lists = []
-    for package_name in args.package_names:
-        install_list_path = os.path.join(
-            workspace_root,
-            subprocess.check_output(
-                [
-                    _BAZEL_PATH,
-                    "cquery",
-                    "--output=files",
-                ]
-                + chrome_prebuilt_configs
-                + [
-                    "@portage//target/%s:install_list" % package_name,
-                ],
-                encoding="utf-8",
-            ).strip(),
-        )
-        with open(install_list_path, encoding="utf-8") as f:
-            install_lists.append(json.load(f))
-
-    # Deduplicate dependencies.
-    deduped_install_list = {}
-    for l in install_lists:
-        for package in l:
-            deduped_install_list.setdefault(package["name"], package)
-
-    # Calculate install groups.
-    remaining_packages = list(deduped_install_list.values())
-    seen = set()
-    install_groups = []
-    while len(remaining_packages) > 0:
-        satisfied_list = []
-        not_satisfied_list = []
-        for package in remaining_packages:
-            if all(dep in seen for dep in package["deps"]):
-                satisfied_list.append(package)
-            else:
-                not_satisfied_list.append(package)
-
-        assert len(satisfied_list) != 0
-        for package in satisfied_list:
-            seen.add(package["name"])
-
-        install_groups.append(satisfied_list)
-        remaining_packages = not_satisfied_list
-
-    # Install binary packages.
-    for group in install_groups:
-        for package in group:
-            src_path = os.path.join(workspace_root, package["path"])
-            dest_path = os.path.join(
-                "/build", args.board, "packages", package["name"]
-            )
-            dest_dir = os.path.dirname(dest_path)
-            subprocess.check_call(["sudo", "mkdir", "-p", dest_dir])
-            subprocess.check_call(["sudo", "cp", src_path, dest_path])
-            subprocess.check_call(["sudo", "chmod", "644", dest_path])
-
-        atoms = ["=%s" % package["name"].rsplit(".", 1)[0] for package in group]
-        subprocess.check_call(
-            ["emerge-%s" % args.board, "--usepkgonly", "--nodeps", "--jobs"]
-            + atoms,
-        )
 
 
 if __name__ == "__main__":
