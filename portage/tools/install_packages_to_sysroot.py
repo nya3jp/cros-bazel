eb530c77505568922dc7ccdf00315900e8bc98cb
Ryo Hashimoto <hashimoto@chromium.org>
2024-01-11 11:40:48 +0000

    install_packages_to_sysroot.py: Generate FAILED_PACKAGES file on failure
    BUG=b:318794206
    TEST=CROS_METRICS_DIR=/tmp/foo bazel build
    @portage//target/chromeos-base/chromeos-chrome
    
    Force-relevant-builds: amd64-generic-bazel-cq
    Change-Id: I564cd55e6676a72176a0a0ae723172eba2fdf6b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5187236
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>

--- portage/tools/install_packages_to_sysroot.py
+++ portage/tools/install_packages_to_sysroot.py
@@ -6,14 +6,75 @@
 """A tool to build and install packages to the sysroot."""
 
 import argparse
+import json
 import logging
 import os
+import re
 import subprocess
+from typing import List
 
 
 _SCRIPT_DIR = os.path.dirname(__file__)
 _SCRIPT_NAME = os.path.basename(__file__)
 _BAZEL_PATH = "/mnt/host/source/chromite/bin/bazel"
+
+_BUILD_EVENT_JSON_FILE_PATH = "/tmp/chromeos_bazel_build_events.json"
+
+
+def _GetFailedPackages(build_event_json_file: str) -> List[str]:
+    """Reads the specified file and returns a list of failed packages.
+
+    Each line of the input file is a JSON object which represents an event, and
+    each event looks like this:
+    {
+      "id": {
+        "actionCompleted": {
+          "primaryOutput": ".../chrome-icu/chrome-icu-122.0.6226.0_rc-r1.tbz2",
+          "label": "@@.../chromeos-base/chrome-icu:122.0.6226.0_rc-r1",
+          "configuration": {
+            "id": "..."
+          }
+        }
+      },
+      "action": {
+        "exitCode": 1,
+        "stderr": {
+          "name": "stderr",
+          "uri": "bytestream://remotebuildexecution.googleapis.com/..."
+        },
+        "label": "@@.../chromeos-base/chrome-icu:122.0.6226.0_rc-r1",
+        "configuration": {
+          "id": "..."
+        },
+        "type": "Ebuild",
+        "commandLine": [
+          ...
+        ],
+        "failureDetail": {
+          "message": "local spawn failed for Ebuild",
+          "spawn": {
+            "code": "NON_ZERO_EXIT",
+            "spawnExitCode": 1
+          }
+        }
+      }
+    }
+    """
+    failed_packages = set()
+    with open(build_event_json_file, encoding="utf-8") as f:
+        for line in f:
+            # Look for actionCompleted events with failureDetails.
+            event = json.loads(line)
+            action_completed = event.get("id", {}).get("actionCompleted")
+            failure_detail = event.get("action", {}).get("failureDetail")
+            if action_completed and failure_detail:
+                label = action_completed.get("label", "")
+                m = re.match(
+                    "@@_main~portage~portage//.*/([^/]+/[^:]+):", label
+                )
+                if m:
+                    failed_packages.add(m.group(1))
+    return list(failed_packages)
 
 
 def main():
@@ -58,27 +119,44 @@ def main():
     logging.info("Chrome prebuilt configs are %s", chrome_prebuilt_configs)
 
     # Build packages and install lists.
-    subprocess.check_call(
-        [
-            _BAZEL_PATH,
-            "build",
-            "--profile=/tmp/allpackages_command.profile.gz",
-            "--experimental_profile_include_target_label",
-            "--experimental_profile_include_primary_output",
-            # --keep_going to keep building packages even after a failure to
-            # detect as many failure as possible on the CI builders.
-            # We may need to delete this after launching Alchemy.
-            "--keep_going",
-            "--execution_log_binary_file=/tmp/allpackages_exec.log",
-            "--noexecution_log_sort",
-            "--config=hash_tracer",
-        ]
-        + chrome_prebuilt_configs
-        + [
-            "@portage//target/%s:installed" % package_name
-            for package_name in args.package_names
-        ],
-    )
+    try:
+        subprocess.check_call(
+            [
+                _BAZEL_PATH,
+                "build",
+                "--profile=/tmp/allpackages_command.profile.gz",
+                "--experimental_profile_include_target_label",
+                "--experimental_profile_include_primary_output",
+                # --keep_going to keep building packages even after a failure to
+                # detect as many failure as possible on the CI builders.
+                # We may need to delete this after launching Alchemy.
+                "--keep_going",
+                "--execution_log_binary_file=/tmp/allpackages_exec.log",
+                "--noexecution_log_sort",
+                "--config=hash_tracer",
+                "--build_event_json_file=%s" % _BUILD_EVENT_JSON_FILE_PATH,
+            ]
+            + chrome_prebuilt_configs
+            + [
+                "@portage//target/%s:installed" % package_name
+                for package_name in args.package_names
+            ],
+        )
+    except subprocess.CalledProcessError:
+        cros_metrics_dir = os.environ.get("CROS_METRICS_DIR")
+        if cros_metrics_dir:
+            failed_packages = _GetFailedPackages(_BUILD_EVENT_JSON_FILE_PATH)
+            if failed_packages:
+                with open(
+                    os.path.join(cros_metrics_dir, "FAILED_PACKAGES"),
+                    "w",
+                    encoding="utf-8",
+                ) as f:
+                    for package in failed_packages:
+                        # "unknown" is a place holder for the failing ebuild
+                        # phase name which won't be used.
+                        f.write("%s unknown\n" % package)
+        raise
 
 
 if __name__ == "__main__":
