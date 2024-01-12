4015891485c39e8af71f93c6f7ff3ab9abc1cc0d
Raul E Rangel <rrangel@chromium.org>
2024-01-12 10:21:59 -0700

    docs: Fix typo in create_sdk_board_root
    BUG=b:317280043
    TEST=Run the command
    
    Change-Id: I9856d2463b2bcf9d3faec5ac91078afadec89a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5192412
    Commit-Queue: Tim Bain <tbain@google.com>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Tim Bain <tbain@google.com>
    Auto-Submit: Raul Rangel <rrangel@chromium.org>
    Tested-by: Raul Rangel <rrangel@chromium.org>

--- portage/bin/alchemist/src/bin/alchemist/alchemist.rs
+++ portage/bin/alchemist/src/bin/alchemist/alchemist.rs
@@ -387,7 +387,7 @@ pub fn alchemist_main(args: Args) -> Result<()> {
                 \t\tYou are running inside the CrOS SDK and `{}` doesn't exist.\n\
                 \n\
                 \t\tPlease run the following command to create the host sysroot and try again:\n\
-                \t\t$ ~/chromiumos/src/chromite/shell/create_sdk_board_root \
+                \t\t$ ~/chromiumos/chromite/shell/create_sdk_board_root \
                 --board {} --profile {}\n\
                 \n\
                 *****",
