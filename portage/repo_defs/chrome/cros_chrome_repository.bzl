nya3jp/cros-bazel

commit 3f22d1a2fbc5d497ee73c1214c2a84d9e6fc56ec

Remove gclient wrapper
Remove gclient wrapper and just run the gclient found in the checkout.
Also stop using depot_tools in the chrome checkout.

BUG=b:322317375
TEST=bazel build @portage//target/chromeos-base/chromeos-chrome

Force-Relevant-Builds: amd64-generic-bazel-cq
Change-Id: Iefe117a907bb69bfeab098ae2a7f54fae49b1753
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5276337
Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
Tested-by: Ryo Hashimoto <hashimoto@chromium.org>
Reviewed-by: Raul Rangel <rrangel@chromium.org>

portage/repo_defs/chrome/cros_chrome_repository.bzl
@@ -67,45 +67,50 @@ def _cros_chrome_repository_impl(ctx):
         ],
     )
 
-    _exec(ctx, [
-        ctx.attr.gclient,
-        "sync",
-        "--noprehooks",
-        "--nohooks",
-        # This unfortunately doesn't change how much data we fetch.
-        "--no-history",
-        # This still pulls in 10000 commits. I wish we could specify the depth.
-        "--shallow",
-        "--jobs",
-        "12",
-    ], "Fetching third_party chromium dependencies")
-
     pwd = _exec(ctx, ["pwd"]).strip()
 
     # Set the cache directories so we don't write to the users home directory.
     cipd_cache_dir = paths.join(pwd, ".cipd-cache")
     vpython_root = paths.join(pwd, ".vpython-root")
-    depot_tools_path = paths.join(pwd, "src/third_party/depot_tools")
+    depot_tools_path = ctx.workspace_root.get_child("chromium/depot_tools")
 
-    # Never auto update depot_tools since we won't have network access.
-    ctx.file(paths.join(depot_tools_path, ".disable_auto_update"), "")
+    _exec(
+        ctx,
+        [
+            depot_tools_path.get_child("gclient"),
+            "sync",
+            "--noprehooks",
+            "--nohooks",
+            # This unfortunately doesn't change how much data we fetch.
+            "--no-history",
+            # This still pulls in 10000 commits. I wish we could specify the depth.
+            "--shallow",
+            "--jobs",
+            "12",
+        ],
+        "Fetching third_party chromium dependencies",
+        PATH = "{}:{}".format(depot_tools_path, ctx.os.environ["PATH"]),
+        CIPD_CACHE_DIR = cipd_cache_dir,
+        VPYTHON_VIRTUALENV_ROOT = vpython_root,
+        DEPOT_TOOLS_UPDATE = "0",
+    )
 
     # This command will populate the cipd cache and create a python venv.
     _exec(
         ctx,
-        [paths.join(depot_tools_path, "ensure_bootstrap")],
+        [depot_tools_path.get_child("ensure_bootstrap")],
         "Downloading depot_tools dependencies",
         PATH = "{}:{}".format(depot_tools_path, ctx.os.environ["PATH"]),
         CIPD_CACHE_DIR = cipd_cache_dir,
         VPYTHON_VIRTUALENV_ROOT = vpython_root,
-        DEPOT_TOOLS_DIR = depot_tools_path,
+        DEPOT_TOOLS_UPDATE = "0",
     )
 
     # Run the hooks with the depot tools pinned by chromium.
     _exec(
         ctx,
         [
-            paths.join(depot_tools_path, "gclient"),
+            depot_tools_path.get_child("gclient"),
             "runhooks",
             "--force",
             "--jobs",
@@ -115,6 +120,7 @@ def _cros_chrome_repository_impl(ctx):
         PATH = "{}:{}".format(depot_tools_path, ctx.os.environ["PATH"]),
         CIPD_CACHE_DIR = cipd_cache_dir,
         VPYTHON_VIRTUALENV_ROOT = vpython_root,
+        DEPOT_TOOLS_UPDATE = "0",
     )
 
     # See https://reproducible-builds.org/docs/archives/
@@ -228,10 +234,6 @@ def _cros_chrome_repository_impl(ctx):
     ctx.template("BUILD.bazel", ctx.attr._build_file)
 
 _cros_sdk_repository_attrs = {
-    "gclient": attr.label(
-        doc = """gclient binary used to fetch chromium.""",
-        mandatory = True,
-    ),
     "internal": attr.bool(
         doc = """If true download Chrome if false download Chromium.""",
         default = False,

module_extensions/cros_deps/depot_tools/BUILD.depot_tools-template
@@ -57,12 +57,3 @@ pkg_tar(
         "@portage//:__subpackages__",
     ],
 )
-
-# By only exposing the file we don't need to depend on vpython, cache
-# files, etc. This does mean that we don't track them as part of the hash
-# though so things won't be rebuilt if one of those changes. Since we only
-# use this as part of a repository rule to fetch code, it shouldn't be a big
-# deal.
-exports_files([
-    "gclient.wrapper.sh",
-])

module_extensions/cros_deps/depot_tools/repositories.bzl
@@ -12,21 +12,8 @@ def depot_tools_repository():
         commit = "86752e9a55281200715749d75a88cf57bf2e7b01",
         shallow_since = "1703067784 +0000",
         patch_cmds = [
-            "touch .disable_auto_update",
-            # We need gclient to fetch the chromium sources
-            '''
-            cat <<-'EOF' > gclient.wrapper.sh
-#!/bin/bash
-ROOT="$(realpath $(dirname "${BASH_SOURCE[0]}"))"
-export CIPD_CACHE_DIR="${ROOT}/.cipd_cache"
-export VPYTHON_VIRTUALENV_ROOT="${ROOT}/.vpython-root"
-export PATH="${ROOT}:$PATH"
-exec "${ROOT}/gclient" "$@"
-EOF
-            ''',
-            "chmod +x gclient.wrapper.sh",
             # Force the cipd binaries and python venv to be downloaded.
-            "DEPOT_TOOLS_DIR=$PWD ./ensure_bootstrap",
+            "DEPOT_TOOLS_UPDATE=0 ./ensure_bootstrap",
         ],
         build_file = "@//bazel/module_extensions/cros_deps:depot_tools/BUILD.depot_tools-template",
     )

portage/bin/alchemist/src/bin/alchemist/generate_repo/deps.rs
@@ -39,11 +39,7 @@ enum Repository {
         tree: String,
     },
     #[allow(clippy::enum_variant_names)]
-    CrosChromeRepository {
-        name: String,
-        tag: String,
-        gclient: String,
-    },
+    CrosChromeRepository { name: String, tag: String },
 }
 
 pub fn generate_deps_file(all_sources: &[&PackageSources], out: &Path) -> Result<()> {
@@ -111,7 +107,6 @@ fn generate_deps(all_sources: &[&PackageSources]) -> Result<Vec<Repository>> {
         .map(|version| Repository::CrosChromeRepository {
             name: format!("chrome-{}", version),
             tag: version.clone(),
-            gclient: "@depot_tools//:gclient.wrapper.sh".to_string(),
         });
 
     Ok(unique_dists.chain(repos).chain(chrome).collect())

commit 6b89a801aa946eb8c67a66956b0a5feb955e895b

portage: Manually create depot_tools .disable_auto_update
If we use `DEPOT_TOOLS_UPDATE`, it will write a timestamp into the
.disable_auto_update file which causes the tarball to be
non-reproducible. Manually create the file to avoid the non-determinism.

BUG=b:322317375, b:322206080
TEST=Generate the tarball twice and compare the hashes.

Fixes: 3f22d1a2 ("Remove gclient wrapper")
Change-Id: I8141eb974e0399c5cd2b8dd4e6474200c29a2e43
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5293954
Commit-Queue: Raul Rangel <rrangel@chromium.org>
Reviewed-by: Tim Bain <tbain@google.com>
Tested-by: Raul Rangel <rrangel@chromium.org>
Auto-Submit: Raul Rangel <rrangel@chromium.org>
Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
Reviewed-by: Justina Raymond <justray778@gmail.com>

portage/repo_defs/chrome/cros_chrome_repository.bzl
@@ -74,6 +74,11 @@ def _cros_chrome_repository_impl(ctx):
     vpython_root = paths.join(pwd, ".vpython-root")
     depot_tools_path = ctx.workspace_root.get_child("chromium/depot_tools")
 
+    # When running hooks `update_depot_tools_toggle.py` will write this file
+    # with a timestamp. By writing it ourselves we can keep the tarball
+    # reproducible.
+    ctx.file(paths.join(pwd, "src/third_party/depot_tools/.disable_auto_update"), "")
+
     _exec(
         ctx,
         [