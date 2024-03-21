nya3jp/cros-bazel

commit 08e166a97bdf75a99b2f36633ce65fee1f11a004

Add a timeout to build_package
BUG=b:327313686
TEST=USE_REMOTEEXEC=true bazel build @portage//target/chromeos-base/chromeos-chrome

Change-Id: I4fb73ca863416e1b97266837f4fcf4ee544a194e
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5343138
Reviewed-by: Tim Bain <tbain@google.com>
Tested-by: Ryo Hashimoto <hashimoto@chromium.org>
Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>

portage/bin/build_package/src/main.rs
@@ -460,8 +460,11 @@ fn do_main() -> Result<()> {
     write_use_flags(&sysroot, &args.ebuild, &args.use_flags)?;
     write_profile_bashrc(&sysroot, &args.bashrc)?;
 
+    // Run ebuild with `timeout 6h` to debug the deadlock issue. b/327313686
     let mut command = container.command(MAIN_SCRIPT);
     command
+        .arg("timeout")
+        .arg("6h")
         .arg("ebuild")
         .arg("--skip-manifest")
         .arg(args.ebuild.mount_path)

commit e41274a0fa8a1750f86ddc865e21408ae1c8ca4c

build_package: Fix Ctrl+C in debug container
`timeout` was creating a new processes group. This means that Ctrl+C
wasn't getting forwarded to the timeout command or the ebuild command.
Passing `--foreground` will avoid creating the new processes group.

(cr) rrangel920 / # ps  --forest xao pid,ppid,pgid,sid,tpgid,tname,args
    PID    PPID    PGID     SID   TPGID TTY      COMMAND
1385699       0 1385699       0 3322094 pts/8    -bash
3322094 1385699 3322094       0 3322094 pts/8     \_ ps --forest xao pid,ppid,pgid,sid,tpgid,tname,args
      1       0       1       0 3266480 pts/3    cros_sdk/32177: pid ns: init
      2       1       2       0 3266480 pts/3    /usr/bin/python3 /home/rrangel/chromiumos/chromite/bin/cros_sdk
     90       2      90       0 3266480 pts/3     \_ -bash
3266480      90 3266480       0 3266480 pts/3         \_ /mnt/host/source/.cache/cipd/packages/skia/bots/bazelisk_${os}_${arch}/bazelisk run -k --config=prebuilts/stage2-brya --@portage//internal/package
3266494 3266480 3266480       0 3266480 pts/3             \_ /home/rrangel/.cache/bazel/_bazel_rrangel/e517ea39ecb43168812d1d21bda62f96/execroot/_main/bazel-out/k8-fastbuild/bin/external/_main~portage~po
3321276 3266494 3266480       0 3266480 pts/3                 \_ bazel-out/k8-opt-exec-ST-a305b725251d/bin/bazel/portage/bin/build_package/build_package --board=brya --ebuild /mnt/host/source/src/third_p
3321438 3321276 3266480       0 3266480 pts/3                     \_ /home/rrangel/.cache/bazel/_bazel_rrangel/e517ea39ecb43168812d1d21bda62f96/execroot/_main/bazel-out/k8-fastbuild/bin/external/_main~po
3321439 3321438 3266480       0 3266480 pts/3                         \_ /bin/cat
3321440 3321438 3266480       0 3266480 pts/3                         \_ /home/rrangel/.cache/bazel/_bazel_rrangel/e517ea39ecb43168812d1d21bda62f96/execroot/_main/bazel-out/k8-fastbuild/bin/external/_mai
3321441 3321440 3266480       0 3266480 pts/3                             \_ /home/rrangel/.cache/bazel/_bazel_rrangel/e517ea39ecb43168812d1d21bda62f96/execroot/_main/bazel-out/k8-fastbuild/bin/external/
3321442 3321441 3266480       0 3266480 pts/3                                 \_ /bin/bash -e /.setup.sh /mnt/host/.build_package/build_package.sh timeout 6h ebuild --skip-manifest /mnt/host/source/src/t
3321443 3321442 3321443       0 3266480 pts/3                                     \_ timeout 6h ebuild --skip-manifest /mnt/host/source/src/third_party/chromiumos-overlay/sys-boot/libpayload/libpayload-9
                ^-- New PGID created without updating TPGID
3321445 3321443 3321443       0 3266480 pts/3                                         \_ /usr/bin/python3.8 -b /usr/lib/python-exec/python3.8/ebuild --skip-manifest /mnt/host/source/src/third_party/chrom
3321979 3321445 3321443       0 3266480 pts/3                                             \_ /bin/bash /usr/lib/portage/python3.8/ebuild.sh compile
3321985 3321979 3321443       0 3266480 pts/3                                                 \_ /bin/bash /usr/lib/portage/python3.8/ebuild.sh compile
3322002 3321985 3321443       0 3266480 pts/3                                                     \_ yes

BUG=b:327313686
TEST=ps --forest xao pid,ppid,pgid,sid,tpgid,tname,args
TEST=BOARD=brya bazel run -@portage//target/sys-boot/libpayload:debug

Fixes: 08e166a9 ("Add a timeout to build_package")

portage/bin/build_package/src/main.rs
@@ -503,6 +503,9 @@ fn do_main() -> Result<()> {
     let mut command = container.command(MAIN_SCRIPT);
     command
         .arg("timeout")
+        // We don't want a new processes group created, otherwise Ctrl+C and
+        // friends won't be forwarded to the ebuild and its children.
+        .arg("--foreground")
         .arg("6h")
         .arg("ebuild")
         .arg("--skip-manifest")