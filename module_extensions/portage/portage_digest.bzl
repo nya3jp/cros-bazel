nya3jp/cros-bazel

commit d7bf5410ba33a20ed8eb75c1f806903c95103097

Add a mechanism to ensure that actions always cache-bust without using environment variables.
This should help us mitigate the effects of github.com/bazelbuild/bazel/issues/20951

BUG=None
TEST=portage/tools/run_tests.sh

Change-Id: I2e6c55e508b75945d0e5f6f1e4e912cc0a369b34
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5420211
Auto-Submit: Matt Stark <msta@google.com>
Tested-by: Matt Stark <msta@google.com>
Reviewed-by: Shuhei Takahashi <nya@chromium.org>
Commit-Queue: Matt Stark <msta@google.com>

module_extensions/portage/portage_digest.bzl
@@ -8,6 +8,7 @@ exports_files(["board", "digest", "profile"])
 
 def _portage_digest_repository_impl(repo_ctx):
     """Repository rule to generate a digest of the boards overlays."""
+    repo_ctx.path(repo_ctx.attr._cache_bust)
 
     # Keep all the ctx.path calls first to avoid expensive restarts
     alchemist = repo_ctx.path(repo_ctx.attr.alchemist)
@@ -53,11 +54,11 @@ portage_digest = repository_rule(
     implementation = _portage_digest_repository_impl,
     environ = [
         # See tools/bazel for where this variable is set
-        "CACHE_BUST_DATE",
         "BOARD",
     ],
     attrs = dict(
         alchemist = attr.label(allow_single_file = True),
+        _cache_bust = attr.label(default = "//bazel:now", allow_single_file = True),
     ),
     local = True,
 )

.gitignore
@@ -5,3 +5,4 @@
 /bazel-*
 /.mypy_cache
 /target/
+/.now

BUILD.bazel
@@ -20,3 +20,11 @@ package_group(
         "//",
     ],
 )
+
+# This file is filled with a timestamp in tools/bazel on each bazel invocation.
+# Thus, depending on this file will ensure that your action always runs.
+filegroup(
+    name = "now",
+    srcs = [".now"],
+    visibility = ["//visibility:public"],
+)

module_extensions/portage/extension.bzl
@@ -9,7 +9,6 @@ any files generated by repos declared in their own module extension (this would
 create circular dependencies). However, they can read files generated by repos
 declared in other module extensions."""
 
-load("//bazel/module_extensions/portage:now.bzl", _now = "now")
 load("//bazel/module_extensions/portage:portage.bzl", _portage = "portage")
 load("//bazel/module_extensions/portage:portage_digest.bzl", "portage_digest")
 load("//bazel/module_extensions/portage:remoteexec_info.bzl", "remoteexec_info")
@@ -31,11 +30,6 @@ def _portage_impl(module_ctx):
     )
     remoteexec_info(
         name = "remoteexec_info",
-    )
-
-    # Used to always cache-bust @portage actions.
-    _now(
-        name = "now",
     )
 
     _portage(

module_extensions/portage/now.bzl
@@ -1,25 +1,0 @@
-# Copyright 2024 The ChromiumOS Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-_NOW_REPO_BUILD_FILE = """
-# Depend on this file if you want a target to always rerun.
-exports_files(["date"])
-"""
-
-def _now_repository_impl(repo_ctx):
-    """Repository rule to generate a file that always changes."""
-
-    repo_ctx.file("BUILD.bazel", content = _NOW_REPO_BUILD_FILE)
-
-    date = repo_ctx.os.environ.get("CACHE_BUST_DATE")
-    repo_ctx.file("date", content = date)
-
-now = repository_rule(
-    implementation = _now_repository_impl,
-    environ = [
-        # See tools/bazel for where this variable is set
-        "CACHE_BUST_DATE",
-    ],
-    local = True,
-)

portage/build_defs/ebuild.bzl
@@ -935,7 +935,7 @@ ebuild_install_action = rule(
             default = Label("//bazel/portage/bin/action_wrapper"),
         ),
         _cache_bust = attr.label(
-            default = Label("@now//:date"),
+            default = Label("//bazel:now"),
             allow_single_file = True,
         ),
     ),

portage/tools/generate-stage2-prebuilts
@@ -81,7 +81,7 @@ do
 
     readarray -t EBUILD_TARGETS < <(
         set -x
-        bazel cquery --repo_env=CACHE_BUST_DATE=none \
+        bazel cquery \
             --//bazel/portage:omit_ebuild_src \
             "kind(ebuild, deps(${TARGET}))" || kill "$$"
     )

workspace_root/alchemy/tools/bazel
@@ -63,11 +63,6 @@ def main(args: List[str]):
         # Let bazel display its own error listing available commands.
         os.execv(bazel_real, [bazel_real, *startup_options])
 
-    # Used to force the alchemist_digest repo rule to re-run every
-    # invocation.
-    cache_bust_date = os.environ.get("CACHE_BUST_DATE", f"{time.time():.9f}")
-    command_options.append(f"--repo_env=CACHE_BUST_DATE={cache_bust_date}")
-
     # Append the current ci.chromium.org build id, if this is a CI build.
     # More info:
     # https://chromium.googlesource.com/infra/luci/luci-py/+/HEAD/client/LUCI_CONTEXT.md
@@ -147,6 +142,8 @@ def main(args: List[str]):
             stderr=subprocess.DEVNULL,
             close_fds=True,
         )
+
+    (workspace / "bazel/.now").write_text(f"{time.time()}")
 
     argv_real = [
         bazel_real,

commit ac984b120ee62796e0869f47cbcb653c9926e85d

portage: Fix invalid cache_bust path
The label for a repository rule must point to a real file, we can't
use a file group.

BUG=none
TEST=modify an ebuild and verify @portage gets regenerated

Fixes: d7bf5410 ("Add a mechanism to ensure that actions always cache-bust without using environment variables.")
Change-Id: Id338ffa9e46865af15d5344e4f0fbc51b139adef
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/5428855
Commit-Queue: Alex Klein <saklein@chromium.org>
Reviewed-by: Alex Klein <saklein@chromium.org>
Reviewed-by: Leandro Lovisolo <lovisolo@google.com>
Tested-by: Raul Rangel <rrangel@chromium.org>
Auto-Submit: Raul Rangel <rrangel@chromium.org>

module_extensions/portage/portage_digest.bzl
@@ -58,7 +58,12 @@ portage_digest = repository_rule(
     ],
     attrs = dict(
         alchemist = attr.label(allow_single_file = True),
-        _cache_bust = attr.label(default = "//bazel:now", allow_single_file = True),
+        _cache_bust = attr.label(
+            # We need to point to the actual file, since we can't use a file
+            # group from a repository rule.
+            default = "//bazel:.now",
+            allow_single_file = True,
+        ),
     ),
     local = True,
 )