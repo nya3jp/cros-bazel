e28cfe1d5438815f26316bb654cee131c4248106
Raul E Rangel <rrangel@google.com>
2025-02-10 13:45:50 -0700

    portage: Set PORTAGE_USERNAME when invoking setup_board
    This variable is normally set by cros_sdk and is used to ensure that
    src_unpack is ran by the same user that ran `repo sync`. Without this
    git complains about using a repo owned by a different user.
    
    BUG=b:394378820
    TEST=BOARD=amd64-generic bazel build @portage//internal/sysroot:all
    
    Change-Id: I1b45b374d505f4a048482aa68fda365003878c23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/bazel/+/6250998
    Auto-Submit: Raul Rangel <rrangel@chromium.org>
    Tested-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>

--- portage/build_defs/setup_board.sh
+++ portage/build_defs/setup_board.sh
@@ -25,6 +25,18 @@ while getopts "b:o:" OPTNAME; do
   esac
 done
 
+# `src_unpack` will run as `${PORTAGE_USERNAME}` if specified, or `portage`
+# otherwise. If we don't set this variable, we get UID mismatches between the
+# `.git` repos and the user running `src_unpack`.
+#
+# Normally `PORTAGE_USERNAME` is set when entering the chroot, but since we
+# are executing as a bazel action, the environment has been stripped of all
+# variables.
+#
+# See b/394378820#comment22.
+export PORTAGE_USERNAME
+PORTAGE_USERNAME="$(id --user --name)"
+
 /mnt/host/source/chromite/bin/setup_board \
   -b "${BOARD}" \
   --reuse-configs \
